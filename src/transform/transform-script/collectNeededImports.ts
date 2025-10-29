974c5cf370ea69146d099b75e5248516ba9147f0
owen <owendells@outlook.com>
2025-10-29 16:04:58 +0800

    refactor: 重构 import 项收集逻辑

--- src/transform/transform-script/collectNeededImports.ts
+++ src/transform/transform-script/collectNeededImports.ts
@@ -1,53 +1,44 @@
 import traverse from '@babel/traverse';
 import * as t from '@babel/types';
-import {
-  REACT_ACTIVATION,
-  REACT_HOOKS,
-  REACT_USE,
-  THIRD_PARTY,
-  USE_IMMER,
-  VUE_TO_REACT_HOOKS,
-} from '@constants/react';
+import { EDDIE_REACT_DEPS, REACT_DEPS, REACT_HOOKS, VUE_TO_REACT_HOOKS } from '@constants/react';
 import type { ScriptTransformContext } from './types';
 import { isTopLevelFunc, normalizeHookName } from './utils';
 
+// TODO 收集工作放到最后，无需通过复杂的表查询
 export function collectNeededImports(ast: t.File, ctx: ScriptTransformContext) {
-  const { neededImports } = ctx;
+  const { imports } = ctx;
   traverse(ast, {
     CallExpression(path) {
       const { callee } = path.node;
       if (!t.isIdentifier(callee)) return;
 
-      const apiName = normalizeHookName(callee.name);
-      if (apiName in VUE_TO_REACT_HOOKS) {
-        const hookName = VUE_TO_REACT_HOOKS[apiName]!;
-        if (hookName in REACT_HOOKS) {
-          neededImports.react.add(hookName);
-        }
-        if (hookName in REACT_USE) {
-          neededImports[THIRD_PARTY.reactUse]
-            .add(hookName)
-            // Conservative import. even if unused, bundlers will auto tree-shaking
-            .add(REACT_USE.useAsync)
-            .add(REACT_USE.useFirstMountState);
-        }
-        if (hookName in REACT_ACTIVATION) {
-          neededImports[THIRD_PARTY.reactActivation].add(hookName);
-        }
-        if (hookName === USE_IMMER) {
-          neededImports[THIRD_PARTY.useImmer] = true;
-          // Simple values ​​require useState and should be introduced import.
-          neededImports.react.add(REACT_HOOKS.useState);
+      const vueApi = normalizeHookName(callee.name);
+
+      if (vueApi in VUE_TO_REACT_HOOKS) {
+        const reactApi = VUE_TO_REACT_HOOKS[vueApi]!;
+
+        if (reactApi in REACT_HOOKS) {
+          imports.react.add(reactApi);
+
+          // We conservatively import useImmer because useState is only used for simple expressions;
+          // complex expressions should be replaced with useImmer.
+          if (reactApi === REACT_HOOKS.useState) {
+            imports[EDDIE_REACT_DEPS].add(REACT_DEPS.useImmer);
+          }
+        } else if (reactApi in REACT_DEPS) {
+          imports[EDDIE_REACT_DEPS].add(reactApi)
+            // Conservative import
+            .add(REACT_DEPS.useAsync)
+            .add(REACT_DEPS.useFirstMountState);
         }
 
-        // 找到匹配的 API 后，跳过当前节点的后续遍历
         path.skip();
       }
     },
+
     'FunctionDeclaration|FunctionExpression|ArrowFunctionExpression'(path) {
       if (isTopLevelFunc(path)) {
-        // Conservative import
-        neededImports.react.add(REACT_HOOKS.useCallback);
+        imports.react.add(REACT_HOOKS.useCallback); // Conservative import
         path.skip();
       }
     },
