ccfa0a91eae2585815cb88a42a73ebd69f5682c7
owen <owendells@outlook.com>
2025-10-29 17:27:16 +0800

    feat: 优化收集工作

--- src/transform/transform-script/collectNeededImports.ts
+++ src/transform/transform-script/collectNeededImports.ts
@@ -1,46 +1,31 @@
 import traverse from '@babel/traverse';
 import * as t from '@babel/types';
-import { EDDIE_REACT_DEPS, REACT_DEPS, REACT_HOOKS, VUE_TO_REACT_HOOKS } from '@constants/react';
+import { EDDIE_REACT_DEPS, REACT_DEPS, REACT_HOOKS } from '@constants/react';
+import { logger } from '@transform/utils/logger';
 import type { ScriptTransformContext } from './types';
-import { isTopLevelFunc, normalizeHookName } from './utils';
+import { normalizeHookName } from './utils';
 
-// TODO 收集工作放到最后，无需通过复杂的表查询
 export function collectNeededImports(ast: t.File, ctx: ScriptTransformContext) {
   const { imports } = ctx;
+
   traverse(ast, {
     CallExpression(path) {
       const { callee } = path.node;
+
       if (!t.isIdentifier(callee)) return;
 
-      const vueApi = normalizeHookName(callee.name);
+      const callName = normalizeHookName(callee.name);
 
-      if (vueApi in VUE_TO_REACT_HOOKS) {
-        const reactApi = VUE_TO_REACT_HOOKS[vueApi]!;
-
-        if (reactApi in REACT_HOOKS) {
-          imports.react.add(reactApi);
-
-          // We conservatively import useImmer because useState is only used for simple expressions;
-          // complex expressions should be replaced with useImmer.
-          if (reactApi === REACT_HOOKS.useState) {
-            imports[EDDIE_REACT_DEPS].add(REACT_DEPS.useImmer);
-          }
-        } else if (reactApi in REACT_DEPS) {
-          imports[EDDIE_REACT_DEPS].add(reactApi)
-            // Conservative import
-            .add(REACT_DEPS.useAsync)
-            .add(REACT_DEPS.useFirstMountState);
-        }
-
-        path.skip();
+      if (callName in REACT_HOOKS) {
+        imports.react.add(callName);
       }
-    },
-
-    'FunctionDeclaration|FunctionExpression|ArrowFunctionExpression'(path) {
-      if (isTopLevelFunc(path)) {
-        imports.react.add(REACT_HOOKS.useCallback); // Conservative import
-        path.skip();
+      if (callName in REACT_DEPS) {
+        imports[EDDIE_REACT_DEPS].add(callName);
       }
+
+      path.skip();
     },
   });
+
+  logger.debug(imports, 'collectNeededImports.ts');
 }
