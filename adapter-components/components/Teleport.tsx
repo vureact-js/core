730a14332ea7a8f9874197a1582127b4c4e3e38f
owen <owendells@outlook.com>
2025-10-30 21:23:57 +0800

    feat: 新增 Teleport 组件

---
+++ adapter-components/components/Teleport.tsx
@@ -0,0 +1,58 @@
+import { memo, useEffect, useState, type PropsWithChildren } from 'react';
+import { createPortal } from 'react-dom';
+
+export interface EddieTeleportProps {
+  /**
+   * Required field. Specifies the target container.
+   * It can be a selector or an actual element.
+   */
+  to: string | HTMLElement;
+  /**
+   * When the value is `true`, the content will remain in its original position,
+   * instead of being moved to the target container.
+   * It can be changed dynamically.
+   */
+  disabled?: boolean;
+  /**
+   * Delay until the current component is mounted
+   */
+  defer?: boolean;
+}
+
+export default memo(EddieTeleport);
+
+function EddieTeleport(props: PropsWithChildren<EddieTeleportProps>) {
+  const { to, disabled, defer, children } = props;
+
+  const [container, setContainer] = useState<Element | null>(null);
+  const [shouldRender, setShouldRender] = useState(!defer);
+
+  useEffect(() => {
+    const el = typeof to === 'string' ? document.querySelector(to) : to;
+    if (!el) {
+      if (typeof to === 'string') {
+        console.error(
+          `[EddieTeleport error] Please check if the selector passed to prop 'to' is correct; Guess '.${to}' or '#${to}'?`,
+        );
+      }
+    } else {
+      setContainer(el);
+    }
+  }, [to]);
+
+  useEffect(() => {
+    if (defer) {
+      Promise.resolve().then(() => setShouldRender(true));
+    }
+  }, [defer]);
+
+  if (!container || disabled) {
+    return <>{children}</>;
+  }
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  return createPortal(children, container);
+}
