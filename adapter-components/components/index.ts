26095377953b909d95fd95e3cf12c5f4f41e3bb3
owen <owendells@outlook.com>
2025-11-03 19:57:10 +0800

    feat: 导出 TransitionGroup 组件

--- adapter-components/components/index.ts
+++ adapter-components/components/index.ts
@@ -1,6 +1,15 @@
 import VueComponent from './Component';
 import VueKeepAlive, { useActived, useDeactived } from './KeepAlive';
 import VueSuspense from './Suspense';
-import VueTransition from './Transition';
+import VueTransition from './Transition/Transition';
+import VueTransitionGroup from './Transition/TransitionGroup';
 
-export { useActived, useDeactived, VueComponent, VueKeepAlive, VueSuspense, VueTransition };
+export {
+  useActived,
+  useDeactived,
+  VueComponent,
+  VueKeepAlive,
+  VueSuspense,
+  VueTransition,
+  VueTransitionGroup,
+};
