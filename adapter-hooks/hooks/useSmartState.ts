184658f4b72ebcc98854f9fb8e5cbefd40b3ff12
smirk9581 <owendells@outlook.com>
2025-11-09 19:51:51 +0800

    feat: 启用 useImmer 对 Map 和 Set 集合类型的支持、优化函数类型推断

--- adapter-hooks/hooks/useSmartState.ts
+++ adapter-hooks/hooks/useSmartState.ts
@@ -1,44 +1,34 @@
-import { type Dispatch, type SetStateAction, useState } from 'react';
-import type { Primitive } from 'types';
-import { type ImmerHook, useImmer } from 'use-immer';
+import { enableMapSet } from 'immer';
+import { useState } from 'react';
+import { useImmer } from 'use-immer';
+import { isObject } from '../utils';
 
-export type SmartStateReturn<S> = [S] extends [Primitive]
-  ? [S, Dispatch<SetStateAction<S>>]
-  : ImmerHook<S>;
+export type SmartStateHook<S> = [S, SmartUpdater<S>];
+
+export type SmartUpdater<S> = (draft: State<S> | UpdateFunction<S>) => void;
+
+export type UpdateFunction<S> = (draft: S) => UpdateFunctionReturnType<S>;
+
+export type UpdateFunctionReturnType<S> = State<S> | void;
+
+export type State<S> = Partial<S>;
 
 /**
- * `useSmartState` will automatically selects the appropriate value type:
- * `useState` for simple values ​​and `useImmer` for complex values.
- * 
- * @example
- * 
- * ```ts 
-   // The setter inference type for simple values ​​is SetStateAction of type useState.
- * const [state, setState] = useSmartState(1);
-
-   // setState: (value: SetStateAction<number>) => void
- * setState(2);
-
-   // The setter inference type for complex values ​​is DraftFunction of useImmer.
- * const [data, updateState] = useSmartState({ name: 'react', list: [] });
-
-   // updateState: (DraftFunction<{name: string; list: never[]}>) => void
- * updateState((draft) => {
-    // You can make any modifications to the draft object.
- *  draft.name = 'eddie';
- *  draft.list.push(1)
- * });
- * ```
- * 
- * `useImmer` documentation
- * @see https://github.com/mweststrate/use-immer#readme
+ * `useSmartState` returns a stateful value and a function to update it.
+ * The update function allows direct modification of the properties and values of the state.
+ *
  */
-export function useSmartState<S>(initialState: S): SmartStateReturn<S>;
+export function useSmartState<S>(initialState: S | (() => S)): SmartStateHook<S>;
 
-export function useSmartState<S>(initialState: () => S): [S, unknown] {
+export function useSmartState<S>(initialState: () => S): SmartStateHook<S>;
+
+export function useSmartState<S>(initialState: S) {
   const state = typeof initialState === 'function' ? initialState() : initialState;
-  if (state !== null && typeof state === 'object') {
-    return useImmer(state) as SmartStateReturn<S>;
+  if (isObject(state)) {
+    return useImmer(state);
   }
-  return useState(state) as SmartStateReturn<S>;
+  return useState(state);
 }
+
+// 启用 useImmer 对 Map 和 Set 集合类型的支持
+enableMapSet();
