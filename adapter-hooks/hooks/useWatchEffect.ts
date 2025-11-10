59f85fe38f36f9dc744f8ae99eb79c049fbf7640
smirk9581 <owendells@outlook.com>
2025-11-10 12:53:42 +0800

    fix: 修复无法手动调用清理函数

--- adapter-hooks/hooks/useWatchEffect.ts
+++ adapter-hooks/hooks/useWatchEffect.ts
@@ -1,25 +1,30 @@
-import { type DependencyList, useEffect, useRef } from 'react';
+import { type DependencyList, useEffect, useMemo, useRef } from 'react';
 import { executeEffect } from '../executeEffect';
 import type { Destructor, EffectCallback } from '../types';
 import { createWatchStopHandle, type WatchStopHandle } from './useWatch';
 
 export function useWatchEffect(fn: EffectCallback, deps?: DependencyList): WatchStopHandle {
   const { stop, onStop } = createWatchStopHandle();
+
   const cleanupRef = useRef<Destructor>(undefined);
+  const newDeps = useMemo(() => [...(deps ?? []), stop], [deps, stop]);
 
   useEffect(() => {
     if (stop) {
-      cleanupRef.current?.();
+      if (cleanupRef.current) {
+        cleanupRef.current();
+        cleanupRef.current = undefined;
+      }
       return;
     }
 
-    executeEffect(fn, (cleanup) => {
+    cleanupRef.current = executeEffect(fn, (cleanup) => {
       cleanupRef.current = cleanup;
     });
 
     return cleanupRef.current;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
+  }, newDeps);
 
   return onStop;
 }
