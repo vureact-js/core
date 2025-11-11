041f19b8fb54d1ba9f8dafa060da5838aae68695
smirk9581 <owendells@outlook.com>
2025-11-11 15:06:03 +0800

    feat: 新增 useWatchPostEffect 和 useWatchSyncEffect

--- adapter-hooks/hooks/useWatchEffect.ts
+++ adapter-hooks/hooks/useWatchEffect.ts
@@ -1,4 +1,4 @@
-import { type DependencyList, useEffect, useMemo, useRef } from 'react';
+import { type DependencyList, useEffect, useLayoutEffect, useMemo, useRef } from 'react';
 import { executeEffect } from '../shared/executeEffect';
 import type { Destructor, EffectCallback } from '../types';
 import { createWatchStopHandle, type WatchStopHandle } from './useWatch';
@@ -7,12 +7,36 @@ import { createWatchStopHandle, type WatchStopHandle } from './useWatch';
  * `useWatchEffect` is similar to Vue's `watchEffect` and returns a stop function.
  */
 export function useWatchEffect(fn: EffectCallback, deps?: DependencyList): WatchStopHandle {
+  return handleEffect(fn, deps, 'post');
+}
+
+/**
+ * `useWatchPostEffect` is similar to Vue's `watchPostEffect` and returns a stop function.
+ */
+export function useWatchPostEffect(fn: EffectCallback, deps?: DependencyList): WatchStopHandle {
+  return handleEffect(fn, deps, 'post');
+}
+
+/**
+ * `useWatchSyncEffect` is similar to Vue's `watchSyncEffect` and returns a stop function.
+ */
+export function useWatchSyncEffect(fn: EffectCallback, deps?: DependencyList): WatchStopHandle {
+  return handleEffect(fn, deps, 'sync');
+}
+
+export function handleEffect(
+  fn: EffectCallback,
+  deps?: DependencyList,
+  timing?: 'post' | 'sync',
+): WatchStopHandle {
   const { stop, onStop } = createWatchStopHandle();
 
   const cleanupRef = useRef<Destructor>(undefined);
-  const newDeps = useMemo(() => [...(deps ?? []), stop], [deps, stop]);
 
-  useEffect(() => {
+  const newDeps = useMemo(() => [...(deps ?? []), stop], [deps, stop]);
+  const effect = useMemo(() => (timing === 'post' ? useEffect : useLayoutEffect), [timing]);
+
+  effect(() => {
     if (stop) {
       if (cleanupRef.current) {
         cleanupRef.current();
@@ -26,7 +50,6 @@ export function useWatchEffect(fn: EffectCallback, deps?: DependencyList): Watch
     });
 
     return cleanupRef.current;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, newDeps);
 
   return onStop;
