41777c7db41c2dfda31a3ebcc67abd82387b4149
Owen Dells <owendells@outlook.com>
2025-11-13 21:51:25 +0800

    feat: 新增 RouteLink 组件及其 Router hooks 导出

--- adapter-components/components/index.ts
+++ adapter-components/components/index.ts
@@ -1,6 +1,18 @@
 import Component from './Component';
 import KeepAlive, { useActived, useDeactived } from './KeepAlive';
+import { RouterLink, useRoute, useRouter } from './Router';
 import Suspense from './Suspense';
 import { Transition, TransitionGroup } from './Transition';
 
-export { Component, KeepAlive, Suspense, Transition, TransitionGroup, useActived, useDeactived };
+export {
+  Component,
+  KeepAlive,
+  RouterLink,
+  Suspense,
+  Transition,
+  TransitionGroup,
+  useActived,
+  useDeactived,
+  useRoute,
+  useRouter,
+};
