4be89d45126f3b4c2791c5b323cc0ae8ebb2df55
Owen Dells <owendells@outlook.com>
2025-11-13 21:53:21 +0800

    feat: 路由模块导出

---
+++ adapter-components/components/Router/index.ts
@@ -0,0 +1,14 @@
+import RouterLink from './components/RouterLink';
+
+export { RouterLink };
+
+export * from './components/RouterLink';
+export * from './creator/createRouter';
+export * from './hooks/useRoute';
+export * from './hooks/useRouter';
+
+export {
+  createMemoryHistory,
+  createWebHashHistory,
+  createWebHistory,
+} from './creator/createHistory';
