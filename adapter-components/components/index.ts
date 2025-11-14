92124d00451f0e225feb3a46610aa81b03c0c3b9
Owen Dells <owendells@outlook.com>
2025-11-14 13:24:42 +0800

    feat: 导出 Router 模块、优化具名导出

--- adapter-components/components/index.ts
+++ adapter-components/components/index.ts
@@ -1,18 +1,9 @@
 import Component from './Component';
-import KeepAlive, { useActived, useDeactived } from './KeepAlive';
-import { RouterLink, useRoute, useRouter } from './Router';
+import KeepAlive from './KeepAlive';
 import Suspense from './Suspense';
-import { Transition, TransitionGroup } from './Transition';
 
-export {
-  Component,
-  KeepAlive,
-  RouterLink,
-  Suspense,
-  Transition,
-  TransitionGroup,
-  useActived,
-  useDeactived,
-  useRoute,
-  useRouter,
-};
+export { Component, KeepAlive, Suspense };
+
+export * from './KeepAlive';
+export * from './Router';
+export * from './Transition';
