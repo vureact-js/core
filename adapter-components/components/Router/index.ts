f508432d5f2f40ecd08688c38d9bf377205040e4
Owen Dells <owendells@outlook.com>
2025-11-14 13:30:31 +0800

    feat: 新增 RouterView 模块导出

--- adapter-components/components/Router/index.ts
+++ adapter-components/components/Router/index.ts
@@ -1,8 +1,10 @@
 import RouterLink from './components/RouterLink';
+import RouterView from './components/RouterView';
 
-export { RouterLink };
+export { RouterLink, RouterView };
 
 export * from './components/RouterLink';
+export * from './components/RouterView';
 export * from './creator/createRouter';
 export * from './hooks/useRoute';
 export * from './hooks/useRouter';
