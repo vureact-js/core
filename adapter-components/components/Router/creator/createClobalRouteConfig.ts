bf3e1129a65403e25a848d96651b7b9f5f1fdddf
Owen Dells <owendells@outlook.com>
2025-11-15 22:31:14 +0800

    feat: 增加重置全局路由配置的函数

--- adapter-components/components/Router/creator/createClobalRouteConfig.ts
+++ adapter-components/components/Router/creator/createClobalRouteConfig.ts
@@ -12,3 +12,8 @@ export function registerRouteConfig(routes: RouteConfig[], convertedRoutes: Rout
   _ROUTE_CONFIG_.source = routes;
   _ROUTE_CONFIG_.converted = convertedRoutes;
 }
+
+export function resetRouteConfig() {
+  _ROUTE_CONFIG_.source = [];
+  _ROUTE_CONFIG_.converted = [];
+}
