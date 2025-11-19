28cb46afc32712a263f9a76f4a23cfa9ac5ec8ca
Owen Dells <owendells@outlook.com>
2025-11-19 14:17:21 +0800

    feat: 新增useBeforeRouteLeave和useBeforeRouteUpdate导出

--- adapter-components/components/Router/index.ts
+++ adapter-components/components/Router/index.ts
@@ -5,9 +5,9 @@ export { RouterLink, RouterView };
 
 export * from './components/RouterLink';
 export * from './components/RouterView';
-export * from './context/RouterContext';
 export * from './creator/createHistory';
 export * from './creator/createRouter';
-export * from './hooks/useGuardManager';
+export * from './hooks/useBeforeRouteLeave';
+export * from './hooks/useBeforeRouteUpdate';
 export * from './hooks/useRoute';
 export * from './hooks/useRouter';
