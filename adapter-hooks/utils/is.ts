728c3e4d99670a2fd5585c70a270d9f468d768c0
smirk9581 <owendells@outlook.com>
2025-11-29 17:57:48 +0800

    feat: is 判断工具

---
+++ adapter-hooks/utils/is.ts
@@ -0,0 +1,30 @@
+import type { AsyncCallback } from '../types';
+
+export function isAsyncFunc(fn: any): fn is AsyncCallback {
+  if (typeof fn !== 'function') {
+    return false;
+  }
+
+  return (
+    fn[Symbol.toStringTag] === 'AsyncFunction' ||
+    Object.prototype.toString.call(fn) === '[object AsyncFunction]' ||
+    fn.constructor.name === 'AsyncFunction' ||
+    isPromise(fn())
+  );
+}
+
+export function isPromise(obj: any): obj is Promise<any> {
+  return (
+    !!obj &&
+    (typeof obj === 'object' || typeof obj === 'function') &&
+    typeof obj.then === 'function'
+  );
+}
+
+export function isObject(value: any): value is object {
+  return value !== null && typeof value === 'object';
+}
+
+export function isPrimitive(value: unknown): boolean {
+  return value === null || (typeof value !== 'object' && typeof value !== 'function');
+}
