5b3ed1b8690e1321a178c6c90abd046e6fa96554
smirk9581 <owendells@outlook.com>
2025-11-29 17:58:41 +0800

    feat: 新增 vShow 工具方法

--- adapter-hooks/utils/vShow.ts
+++ adapter-hooks/utils/vShow.ts
@@ -1,5 +1,8 @@
-type VShowObject = { display: string };
-
-export function vShow(state: boolean): VShowObject {
+/**
+ * vShow - Runtime helper for Vue v-show directive in React JSX
+ * @param state
+ * @returns Object that can be spread as JSX props
+ */
+export function vShow(state: boolean) {
   return { display: state ? '' : 'none' };
 }
