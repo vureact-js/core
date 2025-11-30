ea793578d19657573d09285603da67d0edd9da9a
smirk9581 <owendells@outlook.com>
2025-11-30 13:49:13 +0800

    feat: add tsup.config.ts

---
+++ packages/compile-core/tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'tsup';
+
+const banner = `/**
+ * vureact v1.0.0
+ * (c) 2025-present Owen Dells
+ * @license MIT
+ **/
+`;
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/core/parse/index.ts', 'src/core/transform/index.ts', 'src/core/generate/index.ts'],
+  banner: {
+    js: banner,
+  },
+  outDir: 'lib',
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  external: [
+    '@babel/generator',
+    '@babel/parser',
+    '@babel/traverse',
+    '@babel/types',
+    '@vue/compiler-core',
+    '@vue/compiler-sfc',
+  ],
+  sourcemap: true,
+});
