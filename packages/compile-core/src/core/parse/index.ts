8e55ace8de8124edb244e3f08633c856e53d03a8
smirk9581 <owendells@outlook.com>
2025-12-01 14:24:37 +0800

    feat: vue SFC 解析器

---
+++ packages/compile-core/src/core/parse/index.ts
@@ -0,0 +1,45 @@
+import { ParseResult as BabelParseResult } from '@babel/parser';
+import { logger } from '@shared/logger';
+import { CompilerError, RootNode } from '@vue/compiler-core';
+import {
+  parse as parseSFC,
+  SFCDescriptor,
+  SFCScriptBlock,
+  SFCStyleBlock,
+  SFCTemplateBlock,
+} from '@vue/compiler-sfc';
+import { parseSFCDescriptor } from './parseSFCDescriptor';
+
+export interface ParseOptions {
+  filename: string;
+}
+
+export interface VueASTDescriptor {
+  template: ASTBlock<SFCTemplateBlock, RootNode>;
+  script: ASTBlock<SFCScriptBlock, BabelParseResult>;
+  styles: SFCStyleBlock[];
+  meta: SFCDescriptor;
+}
+
+export type ASTBlock<S, T> = {
+  source: S;
+  ast: T;
+} | null;
+
+export function parse(source: string, options?: Partial<ParseOptions>): VueASTDescriptor {
+  const { descriptor, errors } = parseSFC(source, { filename: options?.filename });
+
+  if (errors.length) {
+    errors.forEach((err) => {
+      logger.error(err.message, {
+        source,
+        file: descriptor.filename,
+        loc: (err as CompilerError).loc,
+      });
+    });
+
+    logger.printAll();
+  }
+
+  return parseSFCDescriptor(descriptor);
+}
