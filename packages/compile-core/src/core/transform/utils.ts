6b3f98342e6b955fd8f5c861f830024282fa9439
smirk9581 <owendells@outlook.com>
2025-12-04 10:36:34 +0800

    feat: 创建 vmodel 事件名

--- packages/compile-core/src/core/transform/utils.ts
+++ packages/compile-core/src/core/transform/utils.ts
@@ -1,3 +1,6 @@
+import { capitalize } from '@utils/capitalize';
 import { ExpressionNode, NodeTypes } from '@vue/compiler-core';
 
 export const isSimpleExp = (node?: ExpressionNode) => node?.type === NodeTypes.SIMPLE_EXPRESSION;
+
+export const createVModelEvName = (modelName: string) => `onUpdate${capitalize(modelName)}`;
