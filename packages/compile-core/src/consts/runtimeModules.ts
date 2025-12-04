7f0d9d973ed852373e7ad331f9c484fefa9bc1e9
smirk9581 <owendells@outlook.com>
2025-12-04 17:21:49 +0800

    feat: 运行时模块名集合

---
+++ packages/compile-core/src/consts/runtimeModules.ts
@@ -0,0 +1,21 @@
+export const enum RuntimeModules {
+  RUNTIME = 'vureact-runtime',
+  RV3_HOOKS = 'react-vue3-hooks',
+  RV3_COMPONENTS = 'react-vue3-components',
+}
+
+export const VR_Runtime = {
+  vOn: 'vOn',
+  vBindCls: 'vBindCls',
+  Memo: 'Memo',
+  Once: 'Once',
+};
+
+export const RV3_Components = {
+  KeepAlive: 'KeepAlive',
+  Suspense: 'Suspense',
+  TransitionGroup: 'TransitionGroup',
+  Transition: 'Transition',
+  Teleport: 'Teleport',
+  Component: 'Component',
+};
