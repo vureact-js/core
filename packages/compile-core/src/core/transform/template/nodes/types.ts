2333ce3c1c0cba050396d1420d0a1913e34034d3
smirk9581 <owendells@outlook.com>
2025-12-05 22:30:47 +0800

    feat: 新增 props NodeTypes 枚举

---
+++ packages/compile-core/src/core/transform/template/nodes/types.ts
@@ -0,0 +1,8 @@
+export const enum NodeTypes {
+  ELEMENT = 'element',
+  TEXT = 'text',
+  COMMENT = 'comment',
+  CONDITIONAL = 'conditional',
+  JSX_EXPRESSION = 'jsxExpression',
+  MapTraversal = 'mapTraversal',
+}
