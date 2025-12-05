0192a527b190eff05da8167a0b977578373716eb
smirk9581 <owendells@outlook.com>
2025-12-05 22:33:16 +0800

    re: 重构 slots 模块

--- packages/compile-core/src/core/transform/template/props/slots.ts
+++ packages/compile-core/src/core/transform/template/props/slots.ts
@@ -1,10 +1,17 @@
-import { DirectiveNode } from '@vue/compiler-core';
-import { BlockTypes, ExpressionBlock, PropsIR } from './index';
-import { createExpressionBlock } from './utils';
+import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
+import { PropTypes } from '.';
+import { ElementNodeIR } from '../nodes/element';
+import { createPropsIR } from './utils';
 
-export type SlotBlock = ExpressionBlock;
+// todo 如何链接插槽内容
+export function handleSlotDirective(prop: DirectiveNode, nodeIR: ElementNodeIR): void {
+  // React 的「插槽」就是 props.children 或任意具名 props.xxx
 
-export function handleSlotDirective(prop: DirectiveNode, propIR: PropsIR): void {
-  const expNodeIR = createExpressionBlock(prop, BlockTypes.SLOT);
-  propIR.slots.push(expNodeIR);
+  const arg = prop.arg as SimpleExpressionNode;
+  const exp = prop.exp as SimpleExpressionNode;
+
+  const slot = createPropsIR(prop.rawName || '', arg.content, exp?.content ?? '', false);
+  slot.type = PropTypes.SLOT;
+
+  nodeIR.props.push(slot);
 }
