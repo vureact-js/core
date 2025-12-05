13d7b5234993de0e0e1aa738f256679dcabb42d6
smirk9581 <owendells@outlook.com>
2025-12-05 22:33:46 +0800

    re: 重构 vhtml 模块

--- packages/compile-core/src/core/transform/template/props/vhtml.ts
+++ packages/compile-core/src/core/transform/template/props/vhtml.ts
@@ -1,16 +1,8 @@
 import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
-import { handleAttributeBlock } from './attributes';
-import { PropsIR } from '.';
+import { ElementNodeIR } from '../nodes/element';
+import { createPropsIR } from './utils';
 
-export function handleVHtml(prop: DirectiveNode, propsIR: PropsIR) {
-  const arg = prop.arg as SimpleExpressionNode;
+export function handleVHtml(prop: DirectiveNode, nodeIR: ElementNodeIR) {
   const exp = prop.exp as SimpleExpressionNode;
-
-  handleAttributeBlock({
-    propsIR,
-    name: 'v-html',
-    value: `{__html: ${exp.content}}`,
-    isStaticKey: arg?.isStatic ?? true,
-    isStaticValue: exp?.isStatic ?? true,
-  });
+  nodeIR.props.push(createPropsIR('v-html', 'v-html', `{__html: ${exp.content}}`, exp.isStatic));
 }
