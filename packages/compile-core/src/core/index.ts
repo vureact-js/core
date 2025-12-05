1056f52a050dd678ca4991742301a60afb6f5022
smirk9581 <owendells@outlook.com>
2025-12-05 22:26:16 +0800

    feat: 添加 compile 函数

--- packages/compile-core/src/core/index.ts
+++ packages/compile-core/src/core/index.ts
@@ -1,2 +1,12 @@
+import { logger } from '@shared/logger';
+
 export * from './parse';
 export * from './transform';
+
+export function compile() {
+  // todo
+
+  if (logger.getLogs().length) {
+    logger.printAll();
+  }
+}
