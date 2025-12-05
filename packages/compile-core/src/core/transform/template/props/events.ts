3a87281fde8b91daeaffee14caf9f3b648480e67
smirk9581 <owendells@outlook.com>
2025-12-05 22:31:15 +0800

    re: 重构 events 模块

---
+++ packages/compile-core/src/core/transform/template/props/events.ts
@@ -0,0 +1,35 @@
+import { strCodeTypes } from '@shared/getStrCodeBabelType';
+import { camelCase } from '@utils/camelCase';
+import { capitalize } from '@utils/capitalize';
+import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
+import { enablePropsRuntimeAssistance } from '../../shared';
+import { ElementNodeIR } from '../nodes/element';
+import { createPropsIR } from './utils';
+
+export function handleEvent(prop: DirectiveNode, nodeIR: ElementNodeIR) {
+  const arg = prop.arg as SimpleExpressionNode;
+  const exp = prop.exp as SimpleExpressionNode;
+
+  let expContent = exp.content.trim();
+
+  // 包装调用表达式 @click="count++" 或者 @click="handleClick"
+  if (strCodeTypes.isCallExpression(expContent) || !strCodeTypes.isSimpleExpression(expContent)) {
+    expContent = `() => ${expContent}`;
+  }
+
+  const name = `on${camelCase(capitalize(arg.content))}`;
+  const event = createPropsIR(prop.rawName!, name, expContent, exp.isStatic);
+
+  event.isStatic = arg.isStatic;
+  event.modifiers = prop.modifiers.map((m) => m.content);
+
+  // 修饰符交给运行时 vOn
+  if (event.modifiers.length) {
+    event.name = `${event.name}.${event.modifiers.join('.')}`;
+    event.value.isBabelParseExp = false;
+
+    enablePropsRuntimeAssistance(event);
+  }
+
+  nodeIR.props.push(event);
+}
