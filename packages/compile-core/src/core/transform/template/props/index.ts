c0838b8369281d731d1315b27cc4daad3846fbbe
smirk9581 <owendells@outlook.com>
2025-12-05 22:31:30 +0800

    re: 重构 transformProps

--- packages/compile-core/src/core/transform/template/props/index.ts
+++ packages/compile-core/src/core/transform/template/props/index.ts
@@ -1,86 +1,57 @@
-import { getContext } from '@core/transform/context';
 import { RuntimeHelper } from '@core/transform/types';
 import { NodeTypes, ElementNode as VueElementNode } from '@vue/compiler-core';
-import { AttributeBlock, handleAttributeBlock } from './attributes';
-import { DirectiveBlock, handleOrdinaryDirective, handleSpecialDirective } from './directives';
-import { EventBindinBlock } from './eventBindings';
-import { SlotBlock } from './slots';
-import { createPropsIR } from './utils';
+import { ElementNodeIR } from '../nodes/element';
+import { handleAttribute } from './attributes';
+import { handleDirective } from './directives';
 
-export interface PropsIR {
-  // 所有静态属性和 v-bind 属性
-  attributes: AttributeBlock[];
-  // 特殊 v-* 指令类 （v-for v-if 等）
-  directives: DirectiveBlock[];
+export interface PropsIR extends RuntimeHelper {
+  type: PropTypes;
+  rawName?: string;
+  name: string;
+  /*
+   非静态键需生成 jsx 对象展开运算符： {...{[name]: value}}
+  */
+  isStatic?: boolean;
+  modifiers?: string[];
   // 事件绑定
-  eventBindings: EventBindinBlock[];
-  // v-slot
-  slots: SlotBlock[];
-}
-
-export type PropBlock = AttributeBlock | ExpressionBlock;
-
-export type ExpressionBlock = BaseBlock &
-  RuntimeHelper & {
-    rawName?: string;
-    name: string;
-    exp: {
-      content: string;
-      /* 完整可用的小段代码，可通过 parseExpression 直接生成 */
-      complete: boolean;
-    };
-    modifiers?: string[];
-    isStatic?: boolean;
+  value: {
+    content: string;
+    /* 临时存储合并项 */
+    combines?: string | string[];
+    /* 
+     默认值 true。
+     可通过 babel parseExpression 直接生成 ast node，
+     如果包含需运行时辅助的则不可用。
+    */
+    isBabelParseExp: boolean;
   };
-
-export interface BaseBlock {
-  type: BlockTypes;
+  /* 
+   无参数 v-bind。
+   生成阶段需配合 type 是否为动态属性，
+   以及 name 是否为空、isKeyLessVBind 是否为 true，
+   如果成立则创建jsx属性表达式 {...{value.content}}
+  */
+  isKeyLessVBind?: boolean;
 }
 
-export enum BlockTypes {
+export enum PropTypes {
   ATTRIBUTE = 1,
   DIRECTIVE = 2,
   SLOT = 3,
   EVENT = 4,
+  DYNAMIC_ATTRIBUTE = 5,
 }
 
-export const specialDirectivePattern = /^v-(if|else|else-if|once|memo|for)$/;
-
-export function transformProps(node: VueElementNode): PropsIR {
-  const propsIR = createPropsIR();
-
+export function transformProps(node: VueElementNode, nodeIR: ElementNodeIR) {
   for (const prop of node.props) {
-    const name = prop.name;
-
     if (prop.type === NodeTypes.ATTRIBUTE) {
-      const value = prop.value?.content || '';
-
-      // 保存 DOM ref
-      if (name === 'ref') {
-        const { nodeRefs } = getContext();
-        nodeRefs.add(value);
-      }
-
-      handleAttributeBlock({
-        name,
-        value,
-        propsIR,
-        isStaticKey: true,
-        isStaticValue: true,
-      });
-
+      handleAttribute(prop, nodeIR);
       continue;
     }
 
     if (prop.type === NodeTypes.DIRECTIVE) {
-      if (specialDirectivePattern.test(prop.rawName ?? name)) {
-        handleSpecialDirective(prop, propsIR);
-        continue;
-      }
-
-      handleOrdinaryDirective(node, prop, propsIR);
+      const stop = handleDirective(node, prop, nodeIR);
+      if (stop) break;
     }
   }
-
-  return propsIR;
 }
