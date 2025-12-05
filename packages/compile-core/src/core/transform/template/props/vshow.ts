29f820cc2a0c9a6d0c6df8722696250520b4f1e3
smirk9581 <owendells@outlook.com>
2025-12-05 22:34:17 +0800

    feat: 新增处理 vshow 的函数

---
+++ packages/compile-core/src/core/transform/template/props/vshow.ts
@@ -0,0 +1,10 @@
+import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
+import { ElementNodeIR } from '../nodes/element';
+import { createPropsIR } from './utils';
+
+export function handleVShow(prop: DirectiveNode, nodeIR: ElementNodeIR) {
+  const exp = prop.exp as SimpleExpressionNode;
+  nodeIR.props.push(
+    createPropsIR('v-show', 'style', `{display: ${exp.content} ? '' : 'none'}`, false),
+  );
+}
