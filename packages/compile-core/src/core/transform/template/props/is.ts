5bfc16778f7e63dc61bddc219db1635a75aae694
smirk9581 <owendells@outlook.com>
2025-12-06 16:01:18 +0800

    feat: 新增静态属性 is 和动态绑定 is 的处理

---
+++ packages/compile-core/src/core/transform/template/props/is.ts
@@ -0,0 +1,40 @@
+import { getContext } from '@core/transform/context';
+import { strCodeTypes } from '@shared/getStrCodeBabelType';
+import { logger } from '@src/shared/logger';
+import { camelCase } from '@utils/camelCase';
+import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
+import { ElementNodeIR } from '../nodes/element';
+import { createPropsIR } from './utils';
+
+export function handleStaticIs(content: string, nodeIR: ElementNodeIR) {
+  if (!content) return;
+
+  if (content.startsWith('vue:')) {
+    const name = content.split('vue:')[1]!;
+    nodeIR.tag = camelCase(name);
+  } else {
+    nodeIR.props.push(createPropsIR('is', 'is', content));
+  }
+}
+
+export function handleDynamicIs(prop: DirectiveNode, nodeIR: ElementNodeIR) {
+  const exp = prop.exp as SimpleExpressionNode;
+
+  if (strCodeTypes.isStringLiteral(exp.content)) {
+    handleStaticIs(exp.content, nodeIR);
+    return;
+  }
+
+  if (strCodeTypes.isIdentifier(exp.content)) {
+    nodeIR.tag = camelCase(exp.content);
+    return;
+  }
+
+  const { source, filename } = getContext();
+
+  logger.error('The expected type of v-bind:is is string or Component (identifier).', {
+    loc: prop.loc,
+    file: filename,
+    source,
+  });
+}
