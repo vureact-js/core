d0a554da3ae68435170165f2eb71b30d44ed9d92
smirk9581 <owendells@outlook.com>
2025-12-07 17:57:44 +0800

    feat: 用于转换vue属性名为react风格

---
+++ packages/runtime-core/src/shared.ts
@@ -0,0 +1,19 @@
+import { camelCase } from './utils/camelCase';
+
+export const vueAttrToReactProp = (name: string): string => {
+  const whitelist = /^data-|datatype|^aria-/;
+
+  switch (name) {
+    case 'v-html':
+      return 'dangerouslySetInnerHTML';
+
+    case 'class':
+      return 'className';
+
+    case 'for':
+      return 'htmlFor';
+
+    default:
+      return whitelist.test(name) ? name : camelCase(name);
+  }
+};
