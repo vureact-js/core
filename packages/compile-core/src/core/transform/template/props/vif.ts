4778e091acfb7f7747e026cea8cb067807fbed2e
smirk9581 <owendells@outlook.com>
2025-12-07 17:58:30 +0800

    feat: 用于处理 v- 条件指令

---
+++ packages/compile-core/src/core/transform/template/props/vif.ts
@@ -0,0 +1,39 @@
+import { getContext } from '@core/transform/context';
+import { logger } from '@shared/logger';
+import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
+import { ElementNodeIR } from '../nodes/element';
+
+export function handleVIf(
+  prop: DirectiveNode,
+  nodeIR: ElementNodeIR,
+  nodesIR: ElementNodeIR[],
+): boolean | void {
+  const exp = prop.exp as SimpleExpressionNode;
+  const name = prop.name === 'else-if' ? 'elseIf' : prop.name;
+
+  // 验证条件分支正确性
+  if (name === 'else' || name === 'elseIf') {
+    const lastNode = nodesIR[nodesIR.length - 1];
+
+    if (lastNode?.meta.conditionalBranch) {
+      const { conditionalBranch } = lastNode.meta;
+
+      if (!conditionalBranch.if && !conditionalBranch.elseIf) {
+        const { source, filename } = getContext();
+
+        logger.error('v-else/v-else-if has no adjacent v-if or v-else-if.', {
+          source,
+          file: filename,
+          loc: prop.loc,
+        });
+
+        return true;
+      }
+    }
+  }
+
+  nodeIR.meta.conditionalBranch = {
+    [name]: true,
+    value: exp?.content,
+  };
+}
