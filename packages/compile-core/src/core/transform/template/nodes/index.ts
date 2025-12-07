27f78c1889b7f896d22a5c8ad1b2d8525eca4a13
smirk9581 <owendells@outlook.com>
2025-12-07 23:43:27 +0800

    feat: 新增文本、注释、插值节点的处理

--- packages/compile-core/src/core/transform/template/nodes/index.ts
+++ packages/compile-core/src/core/transform/template/nodes/index.ts
@@ -1,14 +1,41 @@
-import { NodeTypes, TemplateChildNode } from '@vue/compiler-core';
+import { strCodeTypes } from '@src/shared/getStrCodeBabelType';
+import {
+  SimpleExpressionNode,
+  TemplateChildNode,
+  NodeTypes as VueNodeTypes,
+} from '@vue/compiler-core';
 import { ElementNodeIR, transformElement } from './element';
-import { TextNodeIR } from './text';
+import { createInterpolationNodeIR } from './interpolation';
+import { createTextNodeIR, TextNodeIR } from './text';
 
 export type TemplateChildNodeIR = ElementNodeIR | TextNodeIR;
 
 export function transformChildren(nodes: TemplateChildNode[], result: TemplateChildNodeIR[]) {
   for (const node of nodes) {
-    if (node.type === NodeTypes.ELEMENT) {
+    if (node.type === VueNodeTypes.ELEMENT) {
       const nodeIR = transformElement(node, result as ElementNodeIR[]);
+      
       result.push(nodeIR);
+
+      continue;
+    }
+
+    if (node.type === VueNodeTypes.INTERPOLATION) {
+      const content = (node.content as SimpleExpressionNode).content;
+      const isIdentifier = !strCodeTypes.isStringLiteral(content);
+
+      result.push(createInterpolationNodeIR(content, isIdentifier));
+
+      continue;
+    }
+
+    if (node.type === VueNodeTypes.TEXT) {
+      result.push(createTextNodeIR(node.content));
+      continue;
+    }
+
+    if (node.type === VueNodeTypes.COMMENT) {
+      result.push(createTextNodeIR(node.content, true));
     }
   }
 }
