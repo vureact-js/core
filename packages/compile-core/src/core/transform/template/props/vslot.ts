46e7d2b61f4e8b67391172d52a269f5b52da7d49
smirk9581 <owendells@outlook.com>
2025-12-07 17:56:34 +0800

    feat: 增加 nodesIR 参数

---
+++ packages/compile-core/src/core/transform/template/props/vslot.ts
@@ -0,0 +1,21 @@
+import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
+import { PropTypes } from '.';
+import { ElementNodeIR } from '../nodes/element';
+import { createPropsIR } from './utils';
+
+// todo 如何链接插槽内容
+export function handleVSlot(
+  prop: DirectiveNode,
+  nodeIR: ElementNodeIR,
+  nodesIR: ElementNodeIR[],
+): void {
+  // React 的「插槽」就是 props.children 或任意具名 props.xxx
+
+  const arg = prop.arg as SimpleExpressionNode;
+  const exp = prop.exp as SimpleExpressionNode;
+
+  const slot = createPropsIR(prop.rawName || '', arg.content, exp?.content ?? '', false);
+  slot.type = PropTypes.SLOT;
+
+  nodeIR.props.push(slot);
+}
