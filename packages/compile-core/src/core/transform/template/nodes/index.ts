fe599618923ae33f77d85be9ef270bf4ed29473e
smirk9581 <owendells@outlook.com>
2025-12-07 17:52:36 +0800

    feat: 对 transformElement 透传 result 参数

--- packages/compile-core/src/core/transform/template/nodes/index.ts
+++ packages/compile-core/src/core/transform/template/nodes/index.ts
@@ -2,12 +2,12 @@ import { NodeTypes, TemplateChildNode } from '@vue/compiler-core';
 import { ElementNodeIR, transformElement } from './element';
 import { TextNodeIR } from './text';
 
-export type TemplateChildNodeIR = ElementNodeIR | TextNodeIR
+export type TemplateChildNodeIR = ElementNodeIR | TextNodeIR;
 
 export function transformChildren(nodes: TemplateChildNode[], result: TemplateChildNodeIR[]) {
   for (const node of nodes) {
     if (node.type === NodeTypes.ELEMENT) {
-      const nodeIR = transformElement(node);
+      const nodeIR = transformElement(node, result as ElementNodeIR[]);
       result.push(nodeIR);
     }
   }
