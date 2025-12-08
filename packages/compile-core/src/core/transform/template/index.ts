161c1fd1e85f5a39b417a2f8c425e0e60d598434
smirk9581 <owendells@outlook.com>
2025-12-08 15:33:33 +0800

    feat: 新增和优化

--- packages/compile-core/src/core/transform/template/index.ts
+++ packages/compile-core/src/core/transform/template/index.ts
@@ -1,19 +1,28 @@
-import { RootNode } from '@vue/compiler-core';
+import { RootNode as VueRootNode } from '@vue/compiler-core';
 import { transformChildren } from './nodes';
 import { ElementNodeIR } from './nodes/element';
+import { FragmentNodeIR, wrapWithFragmentIR } from './nodes/fragment';
+import { InterpolationNodeIR } from './nodes/interpolation';
+import { TextNodeIR } from './nodes/text';
 
 export interface TemplateBlockIR {
-  chilren: ElementNodeIR[];
+  chilren: TemplateChildNodeIR[];
 }
 
-export function transformTemplate(root?: RootNode) {
+export type TemplateChildNodeIR = ElementNodeIR | TextNodeIR | InterpolationNodeIR | FragmentNodeIR;
+
+export function transformTemplate(root?: VueRootNode) {
   if (!root) return null;
 
   const block: TemplateBlockIR = {
     chilren: [],
   };
 
-  transformChildren(root.children, block.chilren);
+  transformChildren(root, block.chilren[0] as ElementNodeIR, block.chilren);
+
+  if (block.chilren.length > 1) {
+    block.chilren = wrapWithFragmentIR(block.chilren);
+  }
 
   return block;
 }
