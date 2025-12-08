12e11efd1cccb8fcc2219747e7a98e91f7e43c54
smirk9581 <owendells@outlook.com>
2025-12-08 15:33:00 +0800

    re: 移除 isSimpleExp 工具

--- packages/compile-core/src/core/transform/utils.ts
+++ packages/compile-core/src/core/transform/utils.ts
@@ -1,6 +1,3 @@
 import { capitalize } from '@utils/capitalize';
-import { ExpressionNode, NodeTypes } from '@vue/compiler-core';
-
-export const isSimpleExp = (node?: ExpressionNode) => node?.type === NodeTypes.SIMPLE_EXPRESSION;
 
 export const createVModelEvName = (modelName: string) => `onUpdate${capitalize(modelName)}`;
