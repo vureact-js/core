6ff1f6f4630ab9da5862226cf50c0ede544b0967
smirk9581 <owendells@outlook.com>
2025-12-08 23:18:10 +0800

    re: 移除冗余文件

--- packages/compile-core/src/core/parse/style.ts
+++
@@ -1,6 +0,0 @@
-import { SFCStyleBlock } from '@vue/compiler-sfc';
-import { VueASTDescriptor } from '.';
-
-export function parseStyles(block: SFCStyleBlock[]): VueASTDescriptor['styles'] {
-  return block;
-}
