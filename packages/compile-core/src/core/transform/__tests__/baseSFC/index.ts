dd20a606ccff0a4c3f1fc825e4b3ae9f2560d48b
smirk9581 <owendells@outlook.com>
2025-12-09 14:57:46 +0800

    re: 添加 template 转换结果的日志打印

--- packages/compile-core/src/core/transform/__tests__/baseSFC/index.ts
+++ packages/compile-core/src/core/transform/__tests__/baseSFC/index.ts
@@ -8,5 +8,7 @@ export function transformBaseSFC() {
   const __dirname = getDirname(import.meta.url);
   const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
   const ast = parse(content);
-  transform(ast);
+  const result = transform(ast);
+
+  console.log(JSON.stringify(result.template?.children, null, 2));
 }
