39724438e9025c9bb831007b3f819c46023bb9d0
smirk9581 <owendells@outlook.com>
2025-12-09 15:50:52 +0800

    re: 使用全局babel解析器选项

--- packages/compile-core/src/core/parse/script.ts
+++ packages/compile-core/src/core/parse/script.ts
@@ -1,28 +1,16 @@
-import { ParserPlugin, parse as babelParse } from '@babel/parser';
+import { parse as babelParse } from '@babel/parser';
+import { getBabelParseOptions } from '@shared/babel-utils';
 import { SFCScriptBlock } from '@vue/compiler-sfc';
 import { VueASTDescriptor } from '.';
 
 export function parseScript(block: SFCScriptBlock | null): VueASTDescriptor['script'] {
   if (!block) return null;
 
-  const plugins = (): ParserPlugin[] => {
-    switch (block.lang) {
-      case 'ts':
-        return ['typescript'];
-
-      case 'tsx':
-        return ['typescript', 'jsx'];
-
-      case 'jsx':
-        return ['jsx'];
-
-      default:
-        return [];
-    }
-  };
+  const babelParseOpts = getBabelParseOptions(block.lang as 'js');
+  const ast = babelParse(block.content, babelParseOpts);
 
   return {
     source: block,
-    ast: babelParse(block.content, { sourceType: 'module', plugins: plugins() }),
+    ast,
   };
 }
