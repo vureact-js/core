f219010465dc69fadb0d07e7590a12409eee5615
smirk9581 <owendells@outlook.com>
2025-12-09 17:30:42 +0800

    feat: 共享编译上下文工具

---
+++ packages/compile-core/src/shared/compile-context.ts
@@ -0,0 +1,63 @@
+import { LangType } from './babel-utils';
+
+export interface CompileContextType {
+  source: string;
+  filename: string;
+  /* 收集模板 ref. <div ref="name" /> */
+  nodeRefs: Set<string>;
+  imports: ImportItem[];
+  lang: {
+    script: LangType;
+    style: string[];
+  };
+}
+
+export type ImportItem = {
+  module: string;
+  items: Array<{ name: string; onDemand: boolean }>;
+};
+
+class CompileContext {
+  private ctx: CompileContextType;
+
+  constructor() {
+    this.ctx = this.createContext();
+  }
+
+  get context(): CompileContextType {
+    return this.ctx;
+  }
+
+  createContext = (): CompileContextType => {
+    return {
+      source: '',
+      filename: '',
+      imports: [],
+      nodeRefs: new Set(),
+      lang: {
+        script: 'js',
+        style: [],
+      },
+    };
+  };
+
+  setContext = (opts: Partial<CompileContextType>) => {
+    for (const key in opts) {
+      if (key in this.ctx) {
+        const oldVal = (this.ctx as any)[key];
+        const newVal = (opts as any)[key];
+
+        if (oldVal !== newVal) {
+          (this.ctx as any)[key] = newVal;
+        }
+      }
+    }
+  };
+
+  clear = () => {
+    // @ts-ignore
+    this.ctx = null;
+  };
+}
+
+export const compileContext = new CompileContext();
