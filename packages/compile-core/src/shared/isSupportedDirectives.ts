718a07343c52af18a2b5a3cd1b399a7f6dad336b
smirk9581 <owendells@outlook.com>
2025-12-09 19:52:17 +0800

    re: 移除 isSupportedDirectives

--- packages/compile-core/src/shared/isSupportedDirectives.ts
+++
@@ -1,6 +0,0 @@
-import { SupportedDirectives } from '@consts/supportedDirectives';
-
-export const isSupportedDirectives = (rawName: string = ''): boolean => {
-  if (!rawName.startsWith('v-')) return true;
-  return SupportedDirectives.some((dir) => rawName.match(dir) !== null);
-};
