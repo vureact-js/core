3e69dc0d4baf0511b48b4b163344a4382874be12
smirk9581 <owendells@outlook.com>
2025-12-10 10:24:54 +0800

    re: 文本节点和插值节点的类型接口改为 BaseSimpleNodeIR

--- packages/compile-core/src/core/transform/template/index.ts
+++ packages/compile-core/src/core/transform/template/index.ts
@@ -1,22 +1,21 @@
 import { RootNode as VueRootNode } from '@vue/compiler-core';
-import { transformChildren } from './nodes';
-import { ElementNodeIR } from './nodes/element';
-import { InterpolationNodeIR } from './nodes/interpolation';
-import { TextNodeIR } from './nodes/text';
+import { transformNodes } from './elements';
+import { ElementNodeIR } from './elements/node';
+import { BaseSimpleNodeIR } from './shared/create-simple-node';
 import { FragmentNodeIR, wrapWithFragmentIR } from './shared/fragment';
 
 export interface TemplateBlockIR {
   children: TemplateChildNodeIR[];
 }
 
-export type TemplateChildNodeIR = ElementNodeIR | TextNodeIR | InterpolationNodeIR | FragmentNodeIR;
+export type TemplateChildNodeIR = ElementNodeIR | BaseSimpleNodeIR | FragmentNodeIR;
 
-export function transformTemplate(root?: VueRootNode): TemplateBlockIR | null {
+export function transformRoot(root?: VueRootNode): TemplateBlockIR | null {
   if (!root) return null;
 
   const children: TemplateChildNodeIR[] = [];
 
-  transformChildren(root, children[0] as ElementNodeIR, children);
+  transformNodes(root, children[0] as ElementNodeIR, children);
 
   return {
     children: wrapWithFragmentIR(children),
