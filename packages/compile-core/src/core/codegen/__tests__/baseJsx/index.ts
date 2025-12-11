53248fe9bef095c6e66c8d4e3417acb06113cec5
smirk9581 <owendells@outlook.com>
2025-12-11 17:34:58 +0800

    feat: 添加耗时日志和生成jsx文件

--- packages/compile-core/src/core/codegen/__tests__/baseJsx/index.ts
+++ packages/compile-core/src/core/codegen/__tests__/baseJsx/index.ts
@@ -1,16 +1,24 @@
+import { generate } from '@babel/generator';
 import { generateJsx } from '@core/codegen/jsx';
 import { parse } from '@core/parse';
 import { transform } from '@core/transform';
 import { getDirname } from '@shared/path';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 
 export function baseJsx() {
   const __dirname = getDirname(import.meta.url);
   const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
 
+  console.time('generate jsx duration');
+
   const ast = parse(content);
   const ir = transform(ast);
+  const jsx = generateJsx(ir);
 
-  generateJsx(ir);
+  console.timeEnd('generate jsx duration');
+
+  if (jsx) {
+    writeFileSync(path.resolve(__dirname, './preview.jsx'), generate(jsx).code, 'utf-8');
+  }
 }
