beb5ef27b77a146807e6ac5294e8ae88964f8f58
smirk9581 <owendells@outlook.com>
2025-12-11 17:31:05 +0800

    feat: 新增无参数 v-bind 警告

--- packages/compile-core/src/core/transform/template/shared/check-prop-dynamic-key.ts
+++ packages/compile-core/src/core/transform/template/shared/check-prop-dynamic-key.ts
@@ -4,9 +4,18 @@ import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
 
 export function checkPropIsDynamicKey(prop: DirectiveNode) {
   const isKeyStatic = (prop.arg as SimpleExpressionNode)?.isStatic;
+  const { source, filename } = compileContext.context;
+
+  if (prop.rawName === 'v-bind' && !prop.name) {
+    logger.warn('Keyless v-bind will overwrite all previously declared props at runtime.', {
+      source,
+      loc: prop.arg?.loc,
+      file: filename,
+    });
+    return;
+  }
 
   if (isKeyStatic === false) {
-    const { source, filename } = compileContext.context;
     logger.warn(
       'Failed to analyze dynamic prop. Falling back to source content; please use an explicit prop.',
       {
