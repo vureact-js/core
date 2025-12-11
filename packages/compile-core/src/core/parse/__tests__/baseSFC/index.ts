ba667bcc5a29a1877c0378ad258045549c740f6f
smirk9581 <owendells@outlook.com>
2025-12-11 10:22:59 +0800

    test: 添加执行耗时记录

--- packages/compile-core/src/core/parse/__tests__/baseSFC/index.ts
+++ packages/compile-core/src/core/parse/__tests__/baseSFC/index.ts
@@ -7,7 +7,10 @@ export function parseBaseSFC() {
   const __dirname = getDirname(import.meta.url);
   const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
   const ast = parse(content);
-  console.log('parse result:');
-  console.log('');
+
+  console.time('parse duration');
+
   console.log(ast);
+
+  console.timeEnd('parse duration');
 }
--- packages/compile-core/src/core/transform/__tests__/baseSFC/index.ts
+++ packages/compile-core/src/core/transform/__tests__/baseSFC/index.ts
@@ -7,8 +7,13 @@ import path from 'path';
 export function transformBaseSFC() {
   const __dirname = getDirname(import.meta.url);
   const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
+
+  console.time('parse and transform duration');
+
   const ast = parse(content);
   const result = transform(ast);
+
+  console.timeEnd('parse and transform duration');
 
   console.log(JSON.stringify(result.template?.children, null, 2));
 }
