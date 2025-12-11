5a6b56bcdf744d4477672fdf5aaf841d9fb56b41
smirk9581 <owendells@outlook.com>
2025-12-11 20:01:48 +0800

    feat: 新增共享类型 BabelExp

--- packages/compile-core/src/core/transform/template/shared/types.ts
+++ packages/compile-core/src/core/transform/template/shared/types.ts
@@ -1,7 +1,14 @@
+import { Expression } from '@babel/types';
+
 export const enum NodeTypes {
   FRAGMENT = 0,
   ELEMENT = 1,
   TEXT = 2,
   COMMENT = 3,
   JSX_INTERPOLATION = 4,
+}
+
+export interface BabelExp<T = Expression> {
+  content: string;
+  ast: T;
 }
