c56c7f70caec8c767cb537410a55aeb14408acc6
smirk9581 <owendells@outlook.com>
2025-12-13 00:00:46 +0800

    feat: map 循环构建器

---
+++ packages/compile-core/src/core/codegen/jsx/builders/loop-builder.ts
@@ -0,0 +1,70 @@
+import * as t from '@babel/types';
+import { ElementNodeIR } from '@core/transform/template/elements/node';
+import { convertToExpression } from '../shared';
+import { JSXChild } from '../types';
+import { buildElement } from './element-builder';
+import { buildJSXExpression } from './simple-builder';
+
+export function buildLoop(nodeIR: ElementNodeIR): JSXChild {
+  const loop = nodeIR.meta.loop!;
+
+  loop.isHandled = true;
+
+  // 判断是数组遍历还是对象遍历
+  const isObjectTraversal = loop.value.index && loop.value.key;
+
+  const result = isObjectTraversal
+    ? // 对象遍历 - 使用 Object.entries 或 Object.keys
+      buildObjectLoop(nodeIR)
+    : // 数组遍历
+      buildArrayLoop(nodeIR);
+
+  return result;
+}
+
+function buildArrayLoop(nodeIR: ElementNodeIR): JSXChild {
+  const loop = nodeIR.meta.loop!;
+  const { source, value, index, key } = loop.value;
+
+  // 构建 map 调用的源
+  const sourceIdentifier = t.identifier(source);
+
+  // 构建回调函数参数
+  const params: t.Identifier[] = [t.identifier(value)];
+  if (index ?? key) {
+    params.push(t.identifier((index ?? key)!));
+  }
+
+  // 构建 map 调用
+  const mapCall = t.callExpression(t.memberExpression(sourceIdentifier, t.identifier('map')), [
+    t.arrowFunctionExpression(params, convertToExpression(buildElement(nodeIR)!)),
+  ]);
+
+  return buildJSXExpression(mapCall);
+}
+
+function buildObjectLoop(nodeIR: ElementNodeIR): JSXChild {
+  const loop = nodeIR.meta.loop!;
+  const { source, value, key, index } = loop.value;
+
+  // 对于对象遍历，使用 Object.entries
+  const sourceIdentifier = t.identifier(source);
+  const objectEntries = t.callExpression(
+    t.memberExpression(t.identifier('Object'), t.identifier('entries')),
+    [sourceIdentifier],
+  );
+
+  // 构建回调函数参数
+  const params: t.Identifier[] = [t.identifier(key || 'key'), t.identifier(value)];
+
+  if (index) {
+    params.push(t.identifier(index));
+  }
+
+  // 构建 map 调用
+  const mapCall = t.callExpression(t.memberExpression(objectEntries, t.identifier('map')), [
+    t.arrowFunctionExpression(params, convertToExpression(buildElement(nodeIR)!)),
+  ]);
+
+  return buildJSXExpression(mapCall);
+}
