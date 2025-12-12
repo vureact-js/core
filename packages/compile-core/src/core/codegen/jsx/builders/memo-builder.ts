f9e23b43f21244417fae3ece2543352b2f7fe616
smirk9581 <owendells@outlook.com>
2025-12-13 00:00:19 +0800

    feat: memo 构建器

---
+++ packages/compile-core/src/core/codegen/jsx/builders/memo-builder.ts
@@ -0,0 +1,20 @@
+import { ElementNodeIR } from '@core/transform/template/elements/node';
+import { buildUseMemo } from '@src/core/codegen/script/builders/react-hook-builder';
+import { convertToExpression } from '../shared';
+import { JSXChild } from '../types';
+import { buildElement } from './element-builder';
+import { buildJSXExpression } from './simple-builder';
+
+export function buildMemo(nodeIR: ElementNodeIR): JSXChild {
+  const memo = nodeIR.meta.memo!;
+
+  memo.isHandled = true;
+
+  // 构建 useMemo 调用
+  const deps = memo!.babelExp.ast;
+  const body = convertToExpression(buildElement(nodeIR)!);
+
+  const useMemo = buildUseMemo(body, deps);
+
+  return buildJSXExpression(useMemo);
+}
