d823d4bae61640d6d59c38b79efd5b971a8d05d5
smirk9581 <owendells@outlook.com>
2025-12-12 13:49:04 +0800

    feat: 在 if 和 else if 节点上存储下一个条件分支节点

--- packages/compile-core/src/core/transform/template/props/vif.ts
+++ packages/compile-core/src/core/transform/template/props/vif.ts
@@ -11,35 +11,40 @@ export function handleVIf(
 ): boolean | void {
   const exp = prop.exp as SimpleExpressionNode;
   const name = prop.name === 'else-if' ? 'elseIf' : prop.name;
+  const prevNode = nodesIR[nodesIR.length - 1];
+  const isElseOrElseIf = name === 'else' || name === 'elseIf';
 
   let error = false;
-
   // 验证条件分支正确性
-  if (name === 'else' || name === 'elseIf') {
-    const lastNode = nodesIR[nodesIR.length - 1];
-
-    if (lastNode?.meta.condition) {
-      const { condition } = lastNode.meta;
-
+  if (isElseOrElseIf) {
+    // 查找同级的前一个点是否为 if 分支
+    if (prevNode?.meta.condition) {
+      const { condition } = prevNode.meta;
+      // 非 if & else-if 是错误的
       if (!condition.if && !condition.elseIf) {
         error = true;
       }
     } else {
+      // if分支都没有
       error = true;
     }
   }
 
   if (error) {
     const { source, filename } = compileContext.context;
-
     logger.error('v-else/v-else-if has no adjacent v-if or v-else-if.', {
       source,
       file: filename,
       loc: prop.loc,
     });
-
     return error;
   }
 
   preParseCondition(nodeIR, name, exp?.content ?? 'true');
+
+  // 构建链式节点数据
+  if (prevNode?.meta.condition && isElseOrElseIf) {
+    // 将当前节点保存到前一个if/else if分支的next里
+    prevNode!.meta.condition!.next = nodeIR;
+  }
 }
