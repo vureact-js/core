b0be29b641fdea6658a55f2ccf2a64c073ff540a
smirk9581 <owendells@outlook.com>
2025-12-13 13:39:40 +0800

    添加 todo

--- packages/compile-core/src/core/codegen/index.ts
+++ packages/compile-core/src/core/codegen/index.ts
@@ -1,7 +1,7 @@
 import { ReactIRDescriptor } from '@core/transform';
 import { generateJsx } from './jsx';
 
-// todo
+// todo 配置选项集成：prettier 格式化代码、预设 babel generate 选项
 export function generate(ir: ReactIRDescriptor) {
   generateJsx(ir);
 }
