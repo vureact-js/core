cfa9491a628249b9e25dd1d90fda4677066b6b44
Owen Dells <owendells@outlook.com>
2025-12-13 20:59:32 +0800

    re: 移除需用对象写法的 props 属性，重构为平级任意 prop 的定义，以及接收 children 节点

--- adapter-components/components/Component.tsx
+++ adapter-components/components/Component.tsx
@@ -1,7 +1,7 @@
-import { ComponentType, createElement, JSX, memo, ReactNode, useMemo } from 'react';
+import { ComponentType, createElement, JSX, memo, PropsWithChildren, ReactNode } from 'react';
 import { getReactType } from '../utils';
 
-interface ComponentProps {
+export interface ComponentProps extends Record<string, any> {
   /**
    * It can pass tag names, component functions, and JSX elements.
    */
@@ -9,39 +9,32 @@ interface ComponentProps {
   /**
    * The props passed to the element
    */
-  props?: object | null;
 }
-
-type ReturnType = JSX.Element | null;
 
 export default memo(Component);
 
 /**
- * Equivalent to  dynamic component, with the same usage.
+ * Equivalent to Vue dynamic component, with the same usage.
  */
-function Component({ is, props }: ComponentProps): ReturnType {
-  const renderElement = useMemo(() => {
-    switch (getReactType(is)) {
-      case 'text':
-        return createElement(is as string, props);
+function Component({ is, children, ...anyProps }: PropsWithChildren<ComponentProps>) {
+  switch (getReactType(is)) {
+    case 'text':
+      return createElement(is as string, anyProps, children);
 
-      case 'element':
-        // 传入 JSX 元素 (<CompA />) 直接返回
-        return is;
+    case 'element':
+      // 传入 JSX 元素 (<CompA />) 直接返回
+      return is;
 
-      case 'component':
-        // 传入组件函数 (CompA)
-        const Comp = is as unknown as ComponentType;
-        return <Comp {...props} />;
+    case 'component':
+      // 传入组件函数 (CompA)
+      const Comp = is as unknown as ComponentType<object>;
+      return <Comp {...anyProps}>{children}</Comp>;
 
-      default:
-        // is 可能是 null, undefined 或其他类型
-        console.error(
-          `[Component error] Invalid 'is' prop value or missing component type. -->${is}`,
-        );
-        return null;
-    }
-  }, [is, props]) as ReturnType;
-
-  return renderElement;
+    default:
+      // is 可能是 null, undefined 或其他类型
+      console.error(
+        `[Component error] Invalid 'is' prop value or missing component type. -->${is}`,
+      );
+      return null;
+  }
 }
