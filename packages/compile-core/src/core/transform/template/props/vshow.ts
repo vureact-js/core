cb3ad0d18bf2f0a32d4ce4b2dd17f030fe52474e
smirk9581 <owendells@outlook.com>
2025-12-14 14:11:09 +0800

    feat: 添加至节点 show meta、处理 style 合并

--- packages/compile-core/src/core/transform/template/props/vshow.ts
+++ packages/compile-core/src/core/transform/template/props/vshow.ts
@@ -1,11 +1,33 @@
+import { parseFragmentExp } from '@shared/babel-utils';
 import { DirectiveNode, SimpleExpressionNode } from '@vue/compiler-core';
 import { ElementNodeIR } from '../elements/node';
 import { preParseProp } from '../shared/pre-parse/prop';
+import { findSameProp } from '../shared/utils';
+import { mergePropsIR } from './merge';
 import { createPropsIR } from './utils';
 
 export function handleVShow(prop: DirectiveNode, nodeIR: ElementNodeIR) {
   const exp = prop.exp as SimpleExpressionNode;
-  const propIR = createPropsIR(':style', 'style', `{display: ${exp.content} ? '' : 'none'}`);
-  preParseProp(propIR);
-  nodeIR.props.push(propIR);
+  const test = exp.content;
+  const showIR = createPropsIR('v-show', 'style', `{display: ${test} ? '' : 'none'}`);
+
+  preParseProp(showIR);
+
+  nodeIR.meta.show = {
+    isShow: true,
+    value: test,
+    babelExp: {
+      content: test,
+      ast: parseFragmentExp(test),
+    },
+  };
+
+  const found = findSameProp(nodeIR.props, showIR);
+
+  if (found) {
+    mergePropsIR(found, showIR);
+    return;
+  }
+
+  nodeIR.props.push(showIR);
 }
