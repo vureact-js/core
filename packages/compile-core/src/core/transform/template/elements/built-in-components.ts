a948408371c49f559e65e761bf4ed60d35ea05b6
smirk9581 <owendells@outlook.com>
2025-12-15 14:28:50 +0800

    feat: 智能为 Transition 的节点添加随机 key

--- packages/compile-core/src/core/transform/template/elements/built-in-components.ts
+++ packages/compile-core/src/core/transform/template/elements/built-in-components.ts
@@ -2,9 +2,7 @@ import { RV3_Components } from '@consts/runtimeModules';
 import { compileContext } from '@shared/compile-context';
 import { logger } from '@shared/logger';
 import { SourceLocation } from '@vue/compiler-core';
-import { PropsIR } from '../props';
-import { createPropsIR } from '../props/utils';
-import { preParseProp } from '../shared/pre-parse/prop';
+import { addKeyToNode } from '../shared/utils';
 import { ElementNodeIR } from './node';
 
 export function markBuiltinComponent(nodeIR: ElementNodeIR) {
@@ -21,10 +19,10 @@ export function handleBuiltinComponent(
   if (!parentIR?.isBuiltIn) return;
 
   const { show, condition } = nodeIR.meta;
-  const isTransition = parentIR.tag === 'Transition';
+  const { tag: parentTag, children: parentChildren } = parentIR;
 
   // handle transition component
-  if (isTransition) {
+  if (parentTag === 'Transition') {
     const { source, filename } = compileContext.context;
     const loggerOpts = {
       source,
@@ -32,44 +30,21 @@ export function handleBuiltinComponent(
       file: filename,
     };
 
-    if (parentIR.children.length > 1) {
-      logger.error('<Transition> expected exactly one node.', loggerOpts);
-      return;
-    }
-
-    if (!show && !condition?.if) {
-      const seekParent = parentIR.props.some((p) => p.name === 'if' || p.name === 'show');
-      if (!seekParent) {
-        logger.warn('Missing v-show or v-if; <Transition> requires one of them.', loggerOpts);
+    if (parentChildren.length > 1) {
+      const isAllConditionNodes = parentChildren.every((c) => (c as ElementNodeIR).meta.condition);
+      if (!isAllConditionNodes) {
+        logger.error('<Transition> expected exactly one node.', loggerOpts);
+        return;
       }
+    }
+
+    if (!show && !condition) {
+      logger.warn('Missing v-show or v-if; <Transition> requires one of them.', loggerOpts);
       return;
     }
 
-    if (condition?.next) {
-      logger.error(
-        'When using v-if to switch between multiple nodes, <Transition> effects will not work.',
-        loggerOpts,
-      );
-      return;
-    }
-
-    let propIR = {} as PropsIR;
-
-    if (condition?.if) {
-      propIR = createPropsIR('v-if', 'if', condition.value);
-      nodeIR.meta.condition = undefined;
-    } else if (show?.isShow) {
-      propIR = createPropsIR('v-show', 'show', show.value);
-      propIR.value.babelExp = show.babelExp;
-
-      const idx = nodeIR.props.findIndex((p) => p.rawName === 'v-show');
-      if (idx > -1) nodeIR.props.splice(idx, 1);
-      nodeIR.meta.show = undefined;
-    }
-
-    preParseProp(propIR);
-    parentIR.props.push(propIR);
-
-    return;
+    // add random key to node if necessarily
+    const hasKey = nodeIR.props.some((p) => p.name === 'key');
+    if (!hasKey) addKeyToNode(nodeIR);
   }
 }
