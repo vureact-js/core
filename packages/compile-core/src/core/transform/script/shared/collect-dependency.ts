f5002c8d8abf190229993ca38272a989fe38947a
smirk9581 <owendells@outlook.com>
2025-12-16 16:52:22 +0800

    feat: 收集响应式变量名

--- packages/compile-core/src/core/transform/script/shared/collect-dependency.ts
+++ packages/compile-core/src/core/transform/script/shared/collect-dependency.ts
@@ -1,1 +1,6 @@
-export function collectDependencies() {}
+import { compileContext } from '@shared/compile-context';
+
+export function collectDependency(name: string) {
+  const { dependencies } = compileContext.context;
+  dependencies.add(name);
+}
