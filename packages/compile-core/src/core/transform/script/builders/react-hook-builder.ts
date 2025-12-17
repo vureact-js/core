12e29a6ab1509858ab192fb990548827ec801673
smirk9581 <owendells@outlook.com>
2025-12-17 21:02:25 +0800

    feat: 新增 useReadonly 构建器

--- packages/compile-core/src/core/transform/script/builders/react-hook-builder.ts
+++ packages/compile-core/src/core/transform/script/builders/react-hook-builder.ts
@@ -2,6 +2,16 @@ import * as t from '@babel/types';
 import { RV3_HOOKS } from '@consts/runtimeModules';
 import { CallExpArgs } from '../types';
 
-export function build$useState(_arguments: CallExpArgs) {
-  return t.callExpression(t.identifier(RV3_HOOKS.$useState), _arguments);
+export function buildUseState$(_arguments: CallExpArgs, shallow = false): t.CallExpression {
+  const api = !shallow ? RV3_HOOKS.useState$ : RV3_HOOKS.useShallowState;
+  return t.callExpression(t.identifier(api), _arguments);
+}
+
+export function buildUseReadonly(_arguments: CallExpArgs, shallow = false): t.CallExpression {
+  const api = !shallow ? RV3_HOOKS.useReadonly : RV3_HOOKS.useShallowReadonly;
+  return t.callExpression(t.identifier(api), _arguments);
+}
+
+export function buildUseMemo(_arguments: CallExpArgs): t.CallExpression {
+  return t.callExpression(t.identifier('useMemo'), _arguments);
 }
