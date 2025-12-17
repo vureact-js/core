594e5048ca483b0bf8b5bae955a8e02595b55c09
smirk9581 <owendells@outlook.com>
2025-12-17 21:03:10 +0800

    feat: 增加是否 shallow 判断

--- packages/compile-core/src/core/transform/script/reactive.ts
+++ packages/compile-core/src/core/transform/script/reactive.ts
@@ -2,25 +2,17 @@ import { NodePath, types as t, traverse } from '@babel/core';
 import { RuntimeModules, RV3_HOOKS } from '@consts/runtimeModules';
 import { recordImport } from '@shared/runtime-utils';
 import { ScriptBlockIR } from '.';
-import { build$useState } from './builders/react-hook-builder';
+import { buildUseState$ } from './builders/react-hook-builder';
 import { reactHookVarDecl } from './builders/react-hook-declarator';
 import { checkNodeIsInBlock, getVarKind } from './shared/babel-utils';
-import { collectDependency } from './shared/collect-dependency';
 import { reactiveVarDecl } from './shared/reactive-variable-declarator';
-import { ReactiveTypes } from './shared/utils';
 
-const ADAPT_APIS = {
-  ref: RV3_HOOKS.$useState,
-  reactive: RV3_HOOKS.$useState,
+const adaptApis = {
+  ref: RV3_HOOKS.useState$,
+  reactive: RV3_HOOKS.useState$,
   shallowRef: RV3_HOOKS.useShallowState,
   shallowReactive: RV3_HOOKS.useShallowState,
 } as const;
-
-export const isReactiveApi = (name: string): boolean => name in ADAPT_APIS;
-
-const getAdaptApi = (name?: string) => {
-  return ADAPT_APIS[name as keyof typeof ADAPT_APIS];
-};
 
 export function transformReactive(ast: ScriptBlockIR) {
   traverse(ast, {
@@ -38,26 +30,25 @@ function handleVariableDeclarator(path: NodePath<t.VariableDeclarator>) {
   const varName = reactiveVarDecl.varName();
   const apiName = reactiveVarDecl.apiName();
   const apiArgs = reactiveVarDecl.apiArgs();
-  const adaptApi = getAdaptApi(apiName);
+  const useState$Api = adaptApis[apiName as keyof typeof adaptApis];
 
-  if (!adaptApi) return;
+  if (!useState$Api) return;
 
   checkNodeIsInBlock(path);
-  recordImport(RuntimeModules.RV3_HOOKS, adaptApi, true);
+  recordImport(RuntimeModules.RV3_HOOKS, useState$Api, true);
 
   if (!varName) {
-    path.replaceWith(build$useState(apiArgs));
+    path.replaceWith(buildUseState$(apiArgs));
     return;
   }
 
   const { parameters, annotation } = reactiveVarDecl.apiTSTypes();
-  const newNode = reactHookVarDecl.$useState(kind, varName, apiArgs, {
-    reactiveType: apiName! as ReactiveTypes,
+  const newNode = reactHookVarDecl.useState$(kind, varName, apiArgs, {
+    isShallow: varName.startsWith('shallow'),
     varType: reactiveVarDecl.varType(),
     callTypeParameters: parameters,
     callTypeAnnotation: annotation,
   });
 
   path.replaceWith(newNode);
-  collectDependency(varName);
 }
