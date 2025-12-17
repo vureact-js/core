58731b94679c39d100141835f6cf2b2b5b4dd31a
smirk9581 <owendells@outlook.com>
2025-12-17 20:59:24 +0800

    remove: 移除文件

--- packages/compile-core/src/core/transform/script/shared/collect-dependency.ts
+++
@@ -1,6 +0,0 @@
-import { compileContext } from '@shared/compile-context';
-
-export function collectDependency(name: string) {
-  const { dependencies } = compileContext.context;
-  dependencies.add(name);
-}
