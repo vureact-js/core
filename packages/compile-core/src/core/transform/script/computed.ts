e524d65c66e43989717fe05b1b72283208e14245
smirk9581 <owendells@outlook.com>
2025-12-18 20:54:49 +0800

    feat: 新增 computed 转换逻辑

--- packages/compile-core/src/core/transform/script/computed.ts
+++ packages/compile-core/src/core/transform/script/computed.ts
@@ -1,3 +1,59 @@
+import { NodePath, types as t, traverse } from '@babel/core';
+import { compileContext } from '@shared/compile-context';
+import { RuntimeModules } from '@src/consts/runtimeModules';
+import { logger } from '@src/shared/logger';
+import { recordImport } from '@src/shared/runtime-utils';
 import { ScriptBlockIR } from '.';
+import { buildUseMemo } from './builders/react-hook-builder';
+import { reactHookVarDecl } from './builders/react-hook-variable-declaration';
+import { analyzeFunctionDependencies } from './dependency';
+import { checkNodeIsInBlock } from './shared/babel-utils';
+import { varDeclCallExp } from './shared/destructure-var-decl-call-exp';
+import { ReactiveTypes } from './types';
 
-export function transformComputed(ast: ScriptBlockIR) {}
+const adaptApis = {
+  computed: 'useMemo',
+} as const;
+
+export function transformComputed(ast: ScriptBlockIR) {
+  traverse(ast, {
+    VariableDeclarator: handleVariableDeclarator,
+  });
+}
+
+function handleVariableDeclarator(path: NodePath<t.VariableDeclarator>) {
+  const result = varDeclCallExp.destructure(path);
+  const useMemoApi = adaptApis[result.callExpName as keyof typeof adaptApis];
+
+  if (!useMemoApi) return;
+
+  const [initValue] = result.callExpArgs;
+
+  if (!initValue || !t.isFunction(initValue)) {
+    const { source, filename } = compileContext.context;
+    logger.error('computed must receive a getter function.', {
+      source,
+      loc: initValue!.loc!,
+      file: filename,
+    });
+    return;
+  }
+
+  const deps = analyzeFunctionDependencies(initValue.body, path);
+
+  checkNodeIsInBlock(path);
+  recordImport(RuntimeModules.REACT, useMemoApi, true);
+
+  if (!result.name) {
+    path.replaceWith(buildUseMemo(result.callExpArgs, deps));
+    return;
+  }
+
+  const newNode = reactHookVarDecl.useMemo({
+    ...result,
+    deps,
+    reactiveType: result.callExpName as ReactiveTypes,
+  });
+
+  path.replaceWith(newNode);
+}
