95790add000a44a5d9c885e996e66d927612412e
smirk9581 <owendells@outlook.com>
2025-12-18 20:53:11 +0800

    feat: 新增 ReactiveTypes

--- packages/compile-core/src/core/transform/script/types.ts
+++ packages/compile-core/src/core/transform/script/types.ts
@@ -3,3 +3,15 @@ import * as t from '@babel/types';
 export type VarDeclKind = 'var' | 'let' | 'const' | 'using' | 'await using';
 
 export type CallExpArgs = Array<t.Expression | t.SpreadElement | t.ArgumentPlaceholder>;
+
+export type ReactiveTypes =
+  | 'ref'
+  | 'toRef'
+  | 'toRefs'
+  | 'customRef'
+  | 'shallowRef'
+  | 'reactive'
+  | 'shallowReactive'
+  | 'computed'
+  | 'readonly'
+  | 'shallowReadonly';
