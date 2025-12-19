e6b60372e2dd433327ff8ef83ee22f71e4c098b3
smirk9581 <owendells@outlook.com>
2025-12-19 20:12:30 +0800

    test: 测试代码变更

--- packages/compile-core/src/core/transform/__tests__/baseScript/index.vue
+++ packages/compile-core/src/core/transform/__tests__/baseScript/index.vue
@@ -2,9 +2,20 @@
 // @ts-nocheck
 const count = ref<number | string>([1]);
 
-const obj = reactive({ f: () => 1 });
+const obj = reactive({ f: 1 });
 
 const value = readonly(1);
 
-const double = computed(() => count.value[0] + obj.f());
+const double = computed(() => count.value[0] + obj?.f);
+
+const 函数_易语言 = (参数: number): number => {
+  let 值 = 0;
+
+  const 嵌套函数 = () => {
+    值 += count.value + 参数;
+  };
+
+  嵌套函数();
+  return 值;
+};
 </script>
