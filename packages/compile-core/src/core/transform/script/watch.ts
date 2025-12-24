055e8fc951b64df6f879267226b239d6a97d08f3
smirk9581 <owendells@outlook.com>
2025-12-24 13:36:28 +0800

    feat: 新增 watch 转换

---
+++ packages/compile-core/src/core/transform/script/watch.ts
@@ -0,0 +1,25 @@
+import { NodePath } from '@babel/core';
+import * as t from '@babel/types';
+import { RV3_HOOKS } from '@consts/runtimeModules';
+import { reactHookBuilder } from './builders/react-hook-builder';
+import { setNodeExtensionMeta } from './shared/babel-utils';
+import { requiredCallExpHandling } from './shared/required-before-transform';
+
+const adaptApis = {
+  watch: RV3_HOOKS.useWatch,
+} as const;
+
+export function transformWatch(path: NodePath<t.CallExpression>) {
+  const result = requiredCallExpHandling(path, adaptApis, { checkHookArgs: false });
+
+  if (!result) return;
+
+  const { args } = result;
+
+  // 变量声明的 watch 返回值标记为间接响应式
+  if (t.isVariableDeclarator(path.parent)) {
+    setNodeExtensionMeta(path.parent, { isReactive: true, reactiveType: 'indirect' });
+  }
+
+  path.replaceWith(reactHookBuilder.useWatch(args));
+}
