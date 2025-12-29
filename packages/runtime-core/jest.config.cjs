6312e4804896553f4b85485b7c85de4eba6c5359
smirk9581 <owendells@outlook.com>
2025-12-29 16:52:16 +0800

    feat: 更新 jest 配置

--- packages/runtime-core/jest.config.cjs
+++ packages/runtime-core/jest.config.cjs
@@ -1,9 +1,19 @@
 module.exports = {
-  displayName: "runtime-core",
-  testEnvironment: "jsdom",
+  displayName: 'runtime-core',
+
+  preset: 'ts-jest',
+
+  testEnvironment: 'jsdom',
+
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  testMatch: ["<rootDir>/src/**/__tests__/**/*.(test|spec).(ts|tsx)"],
+
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
+
+  transformIgnorePatterns: ['/node_modules/'],
+
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.(test|spec).(ts|tsx)'],
 };
