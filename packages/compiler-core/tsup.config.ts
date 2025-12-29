545e3a08bee4e3b6b9910045034200e16562f9b3
smirk9581 <owendells@outlook.com>
2025-12-29 21:17:39 +0800

    chore: 取消分包构建、代码压缩、代码分割

--- packages/compiler-core/tsup.config.ts
+++ packages/compiler-core/tsup.config.ts
@@ -1,31 +1,26 @@
 import { defineConfig } from 'tsup';
 
 const banner = `/**
- * vureact/compiler-core v1.0.0
- * (c) 2025-present Owen Dells
+ * @vureact/compiler-core v1.0.0
+ * (c) 2025-present Ryan John
  * @license MIT
- **/
+ */
 `;
 
 export default defineConfig({
   tsconfig: './tsconfig-build.json',
-  // 关键修改：使用对象映射入口，确保输出文件名与 package.json 对应
   entry: {
-    index: 'src/index.ts',
-    'parse/index': 'src/core/parse/index.ts',
-    'transform/index': 'src/core/transform/index.ts',
-    'codegen/index': 'src/core/codegen/index.ts',
+    'compiler-core': 'src/index.ts',
   },
   banner: {
     js: banner,
   },
   outDir: 'lib',
   format: ['cjs', 'esm'],
-  // 自动根据 entry 生成对应的 .d.ts 文件
   dts: true,
   clean: true,
-  splitting: true, // 开启代码分割，提取公共逻辑
-  minify: true, // 编译器代码建议开启压缩
+  splitting: false,
+  minify: false,
   external: [
     '@babel/generator',
     '@babel/parser',
