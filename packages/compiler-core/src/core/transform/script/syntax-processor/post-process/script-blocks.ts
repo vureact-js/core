bc6a207dff62e5c0a42b5a80e09fbc2e811f5b74
smirk9581 <owendells@outlook.com>
2025-12-30 15:30:18 +0800

    feat: 分割导出声明和顶级 TS 类型声明

--- packages/compiler-core/src/core/transform/script/syntax-processor/post-process/script-blocks.ts
+++ packages/compiler-core/src/core/transform/script/syntax-processor/post-process/script-blocks.ts
@@ -1,5 +1,6 @@
 import { ParseResult } from '@babel/core';
 import { TraverseOptions } from '@babel/traverse';
+import * as t from '@babel/types';
 import { __scriptBlockIR } from '../..';
 
 export function splitScriptBlocks(): TraverseOptions {
@@ -9,7 +10,21 @@ export function splitScriptBlocks(): TraverseOptions {
       path.remove();
     },
 
-    TSInterfaceDeclaration(path) {},
+    ExportDeclaration(path) {
+      __scriptBlockIR.exports.push(path.node);
+      path.remove();
+    },
+
+    // 处理顶级类型声明
+    'TSInterfaceDeclaration|TSTypeAliasDeclaration|TSEnumDeclaration|TSModuleDeclaration|TSModuleDeclaration'(
+      path,
+    ) {
+      // 确保是顶级声明（父节点是 Program）
+      if (t.isProgram(path.parent)) {
+        __scriptBlockIR.tsTypes.push(path.node as t.TypeScript);
+        path.remove();
+      }
+    },
   };
 }
 
