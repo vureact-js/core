df4ba868e13949ba81ecb40eea52dd97b7553cde
smirk9581 <owendells@outlook.com>
2025-12-31 15:34:50 +0800

    re: slot.ts 更名为 template-vslot.ts

---
+++ packages/compiler-core/src/core/transform/template/elements/template-vslot.ts
@@ -0,0 +1,19 @@
+import { NodeTypes, ElementNode as VueElementNode } from '@vue/compiler-core';
+import { transformNodes } from '.';
+import { handleVSlot, SlotPropsIR } from '../props/vslot';
+import { ElementNodeIR } from './node';
+
+export function transformVSlotNode(node: VueElementNode, nodeIR: ElementNodeIR) {
+  let slotPropIR = {} as SlotPropsIR;
+
+  for (const prop of node.props) {
+    if (prop.type === NodeTypes.DIRECTIVE) {
+      slotPropIR = handleVSlot(prop);
+    }
+  }
+
+  // 转换 <template v-slot> 的子节点内容
+  transformNodes(node, nodeIR, slotPropIR.callback.exp);
+
+  nodeIR.props.push(slotPropIR);
+}
