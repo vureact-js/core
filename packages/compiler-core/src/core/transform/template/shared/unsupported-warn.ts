9ee8b0c50833d42b7f4619510b7bf9f9f7e242a5
smirk9581 <owendells@outlook.com>
2026-01-03 15:01:45 +0800

    feat: 模板通用的不支持警告

---
+++ packages/compiler-core/src/core/transform/template/shared/unsupported-warn.ts
@@ -0,0 +1,44 @@
+import { compileContext } from '@src/shared/compile-context';
+import { logger } from '@src/shared/logger';
+import { DirectiveNode, InterpolationNode, NodeTypes } from '@vue/compiler-core';
+
+export function warnUnsupportedDirective(loc: any, rawName?: string) {
+  const { source, filename } = compileContext.context;
+  logger.warn(`Unsupported or unknown directive: ${rawName}`, {
+    loc,
+    source,
+    file: filename,
+  });
+}
+
+export function warnVueDollarVar(node: DirectiveNode | InterpolationNode) {
+  const { source, filename } = compileContext.context;
+
+  let value = '';
+  let loc;
+
+  if (node.type === NodeTypes.DIRECTIVE && node.exp?.type === NodeTypes.SIMPLE_EXPRESSION) {
+    value = node.exp.content;
+    loc = node.exp?.loc;
+  } else if (
+    node.type === NodeTypes.INTERPOLATION &&
+    node.content.type === NodeTypes.SIMPLE_EXPRESSION
+  ) {
+    value = node.content.content;
+    loc = node.loc;
+  }
+
+  const dollar = ['$attrs', '$slots', '$refs', '$emit', '$props'].find((d) => value.includes(d));
+
+  if (dollar) {
+    logger.error(
+      `Unsupported template variable "${dollar}". ` +
+        `Runtime-injected $ variables are intentionally not supported.`,
+      {
+        source,
+        loc,
+        file: filename,
+      },
+    );
+  }
+}
