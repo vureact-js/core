621acefe9f845b5f9219b0bddd41ba6397d958f3
smirk9581 <owendells@outlook.com>
2026-01-04 13:03:18 +0800

    feat: 用于替换 .vue 文件名后缀为 .jsx/.tsx

---
+++ packages/compiler-core/src/core/transform/script/shared/replace-vue-suffix.ts
@@ -0,0 +1,17 @@
+import { StringLiteral } from '@babel/types';
+import { compileContext } from '@src/shared/compile-context';
+
+/**
+ * 替换 .vue 文件名后缀为 .jsx/.tsx
+ * 
+ * @param node babel 字符串节点
+ */
+export function replaceVueSuffix(node: StringLiteral) {
+  if (!node.value.endsWith('.vue')) return;
+
+  const { lang } = compileContext.context;
+  const jsxFile = node.value.replace(/.vue$/, `.${lang.script}x`);
+
+  node.value = jsxFile;
+  node.extra = { rawValue: jsxFile, raw: jsxFile };
+}
