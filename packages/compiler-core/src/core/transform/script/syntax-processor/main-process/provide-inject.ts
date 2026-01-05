6a2162f00e3b66d7536d8d24d57d2555eeb6cdbd
smirk9581 <owendells@outlook.com>
2026-01-05 14:53:30 +0800

    feat: 新增对 provide api 的转换处理

--- packages/compiler-core/src/core/transform/script/syntax-processor/main-process/provide-inject.ts
+++ packages/compiler-core/src/core/transform/script/syntax-processor/main-process/provide-inject.ts
@@ -1,8 +1,11 @@
+import { generate } from '@babel/generator';
 import { NodePath, TraverseOptions } from '@babel/traverse';
 import * as t from '@babel/types';
 import { RuntimeModules, VuR_Runtime } from '@src/consts/runtimeModules';
+import { compileContext, CompileContextType } from '@src/shared/compile-context';
 import { recordImport } from '@src/shared/runtime-utils';
 import { isCalleeNamed, replaceCallName } from '../../shared/babel-utils';
+import { CallExpArgs } from '../../shared/types';
 import { warnVueHookInAnyCallback, warnVueHookInBlock } from '../../shared/unsupported-warn';
 
 export function resolveProvideInject(): TraverseOptions {
@@ -23,6 +26,63 @@ function processProvideApi(path: NodePath<t.CallExpression>) {
 
   warnVueHookInBlock(path);
   warnVueHookInAnyCallback(path);
+
+  const { ctxProvider } = compileContext.context;
+
+  const key = node.arguments[0];
+  const value = node.arguments[1];
+
+  const target = findOrCreateCtxProvider(ctxProvider);
+  assignProviderValue(target, key, value);
+
+  path.parentPath.remove();
+}
+
+function findOrCreateCtxProvider(root: CompileContextType['ctxProvider']) {
+  if (!root.exists) {
+    return root;
+  }
+
+  let cur = root.ctxProvider;
+  while (cur?.exists) {
+    cur = cur.ctxProvider ?? {};
+  }
+
+  return cur || (root.ctxProvider = {});
+}
+
+function assignProviderValue(
+  target: Partial<CompileContextType['ctxProvider']>,
+  key?: CallExpArgs[0],
+  value?: CallExpArgs[0],
+) {
+  const getRawExp = (exp?: CallExpArgs[0]): string => {
+    if (!exp) return '""'; // 空字符串
+
+    if (t.isStringLiteral(exp)) {
+      return `"${exp.value}"`;
+    }
+
+    if (t.isNumericLiteral(exp)) {
+      return exp.value.toString();
+    }
+
+    if (t.isIdentifier(exp)) {
+      return exp.name;
+    }
+
+    try {
+      // 降级：直接使用源码
+      return generate(exp).code;
+    } catch {
+      return 'null';
+    }
+  };
+
+  target.exists = true;
+  target.name = getRawExp(key);
+  target.value = getRawExp(value);
+  target.ctxProvider = {};
 }
 
 function processInjectApi(path: NodePath<t.CallExpression>) {
