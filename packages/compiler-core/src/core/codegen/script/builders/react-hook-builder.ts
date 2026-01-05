cda11d01e76ff2e8640affc88e3505ed5bd86da6
smirk9581 <owendells@outlook.com>
2026-01-05 20:15:00 +0800

    remove: 移除 react-hook-builder.ts

--- packages/compiler-core/src/core/codegen/script/builders/react-hook-builder.ts
+++
@@ -1,5 +0,0 @@
-import * as t from '@babel/types';
-
-export function buildUseMemo(body: t.Expression, deps: t.ArrayExpression) {
-  return t.callExpression(t.identifier('useMemo'), [t.arrowFunctionExpression([], body), deps]);
-}
