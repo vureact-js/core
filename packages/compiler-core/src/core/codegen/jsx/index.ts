d2761f1fd7adb7337dc86bf1990ade8ec3b9dfa3
smirk9581 <owendells@outlook.com>
2026-01-05 14:54:57 +0800

    feat: 增加使用上下文组件包裹

--- packages/compiler-core/src/core/codegen/jsx/index.ts
+++ packages/compiler-core/src/core/codegen/jsx/index.ts
@@ -1,5 +1,7 @@
 import { ReactIRDescriptor } from '@core/transform';
 import { TemplateChildNodeIR } from '@core/transform/template';
+import { compileContext } from '@src/shared/compile-context';
+import { buildCtxProvider } from './builders/ctx-provider-builder';
 import { buildElement } from './builders/element-builder';
 import { buildFragment } from './builders/simple-builder';
 import { JSXChild } from './types';
@@ -21,6 +23,12 @@ export function buildChildren(
   const jsx = children.map(buildElement).filter(Boolean) as JSXChild[];
 
   if (isRoot) {
+    const { ctxProvider } = compileContext.context;
+
+    if (ctxProvider.exists) {
+      return buildCtxProvider(ctxProvider, jsx);
+    }
+
     return buildFragment(jsx);
   }
 
