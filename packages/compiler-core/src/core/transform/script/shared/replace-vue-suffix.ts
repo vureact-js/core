d99a1502cdcc2cc2940e7e1e2d94af9e69826bca
smirk9581 <owendells@outlook.com>
2026-01-06 10:54:12 +0800

    fix: 修复丢失双引号字符串

--- packages/compiler-core/src/core/transform/script/shared/replace-vue-suffix.ts
+++ packages/compiler-core/src/core/transform/script/shared/replace-vue-suffix.ts
@@ -3,14 +3,14 @@ import { compileContext } from '@src/shared/compile-context';
 
 /**
  * 替换 .vue 文件名后缀为 .jsx/.tsx
- * 
+ *
  * @param node babel 字符串节点
  */
 export function replaceVueSuffix(node: StringLiteral) {
   if (!node.value.endsWith('.vue')) return;
 
   const { lang } = compileContext.context;
-  const jsxFile = node.value.replace(/.vue$/, `.${lang.script}x`);
+  const jsxFile = `"${node.value.replace(/.vue$/, `.${lang.script}x`)}"`;
 
   node.value = jsxFile;
   node.extra = { rawValue: jsxFile, raw: jsxFile };
