4ad8abbf11edd5377f11fcc9587fb4eb674978d4
smirk9581 <owendells@outlook.com>
2026-01-06 16:39:32 +0800

    feat: 新增常量

--- packages/compiler-core/src/core/transform/const.ts
+++ packages/compiler-core/src/core/transform/const.ts
@@ -1,3 +1,6 @@
 export const __props = '__props';
 export const __emits = '__emits';
-export const ReactCompProps = 'ReactComponentProps';
+export const ReactCompProps = 'ReactCompProps';
+export const ReactCompEvents = 'ReactCompEvents';
+export const ReactCompSlots = 'ReactCompSlots';
+export const PropsIntersectionType = 'PropsIntersectionType';
