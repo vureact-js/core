6577ec0a8aff5e118ecd12d4139d2753f0b2f73f
smirk9581 <owendells@outlook.com>
2026-01-09 15:10:35 +0800

    re: getBabelParseOptions 添加文件名参数

--- packages/compiler-core/src/core/parse/script.ts
+++ packages/compiler-core/src/core/parse/script.ts
@@ -3,14 +3,20 @@ import { getBabelParseOptions } from '@shared/babel-utils';
 import { SFCScriptBlock } from '@vue/compiler-sfc';
 import { VueASTDescriptor } from '.';
 
-export function parseScript(block: SFCScriptBlock | null): VueASTDescriptor['script'] {
+export function parseScript(
+  block: SFCScriptBlock | null,
+  filename?: string,
+): VueASTDescriptor['script'] {
   if (!block) return null;
 
-  const babelParseOpts = getBabelParseOptions(block.lang as 'js', 'vueTemplate', '');
-  const ast = babelParse(block.content, babelParseOpts);
+  const options = getBabelParseOptions(
+    block.lang as 'js',
+    'vueTemplate',
+    filename || 'anonymous.vue',
+  );
 
   return {
     source: block,
-    ast,
+    ast: babelParse(block.content, options),
   };
 }
