a1dc6ea5de54b133f8dcdd288e7588ed0876158d
smirk9581 <owendells@outlook.com>
2026-01-09 15:08:59 +0800

    re: 优化测试代码

--- packages/compiler-core/src/core/parse/__tests__/baseSFC/index.ts
+++ packages/compiler-core/src/core/parse/__tests__/baseSFC/index.ts
@@ -1,3 +1,4 @@
+import { createCompilationCtx } from '@compiler/context';
 import { getDirname } from '@shared/path';
 import { readFileSync } from 'fs';
 import path from 'path';
@@ -6,11 +7,19 @@ import { parse } from '../../';
 export function parseBaseSFC() {
   const __dirname = getDirname(import.meta.url);
   const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
-  const ast = parse(content);
+
+  const ctx = createCompilationCtx();
+  ctx.init({ filename: './index.vue', source: content });
 
   console.time('parse duration');
 
-  console.log(ast);
+  const ast = parse(content, ctx.data);
 
   console.timeEnd('parse duration');
+
+  console.log('\n=============== Compilation context data: ===============\n');
+  console.log(ctx.data);
+
+  console.log('\n======================== SFC AST ========================: \n');
+  console.log(ast);
 }
