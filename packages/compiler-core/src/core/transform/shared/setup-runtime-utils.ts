91c4e943348f3701c5655bdf771ba56e6e8ef1be
smirk9581 <owendells@outlook.com>
2026-01-24 20:17:16 +0800

    re: 只保留 recordImport 函数

--- packages/compiler-core/src/core/transform/shared/setup-runtime-utils.ts
+++ packages/compiler-core/src/core/transform/shared/setup-runtime-utils.ts
@@ -1,49 +1,5 @@
 import { ICompilationContext } from '@compiler/context/types';
-import { RuntimeModules, VuR_Runtime } from '@consts/runtimeModules';
-
-export function clsRuntime(ctx: ICompilationContext, arg: string, merge?: string): string {
-  const fnName = VuR_Runtime.vBindCls;
-  const fnArgs = [arg, merge].filter(Boolean).join(',');
-
-  recordImport(ctx, RuntimeModules.VUREACT_RUNTIME, fnName, true);
-
-  return `${fnName}(${fnArgs})`;
-}
-
-export function styleRuntime(
-  ctx: ICompilationContext,
-  target: string,
-  ...merges: string[]
-): string {
-  const fnName = VuR_Runtime.vBindStyle;
-  const fnArgs = [target, ...merges].filter(Boolean).join(',');
-
-  recordImport(ctx, RuntimeModules.VUREACT_RUNTIME, fnName, true);
-
-  return `${fnName}(${fnArgs})`;
-}
-
-export function vOnRuntime(ctx: ICompilationContext, event: string, handler: string): string {
-  const fnName = VuR_Runtime.vOn;
-  const fnArgs = [event, handler].filter(Boolean).join(',');
-
-  recordImport(ctx, RuntimeModules.VUREACT_RUNTIME, fnName, true);
-
-  return `${fnName}(${fnArgs})`;
-}
-
-export function vBindRuntime(ctx: ICompilationContext, strObj: string): string {
-  const fnName = VuR_Runtime.vBind;
-  recordImport(ctx, RuntimeModules.VUREACT_RUNTIME, fnName, true);
-
-  return `${fnName}(${strObj})`;
-}
-
-export function IsComponent(ctx: ICompilationContext): 'Component' {
-  const comp = VuR_Runtime.Component;
-  recordImport(ctx, RuntimeModules.VUREACT_RUNTIME, comp, true);
-  return comp;
-}
+import { RuntimeModules } from '@consts/runtimeModules';
 
 export function recordImport(
   ctx: ICompilationContext,
