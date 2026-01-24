9f6f785af6ee1949316cfbadd879997c308dcb37
smirk9581 <owendells@outlook.com>
2026-01-24 13:08:05 +0800

    feat: 新增 Adapter 类

--- packages/runtime-core/src/adapter-utils/index.ts
+++ packages/runtime-core/src/adapter-utils/index.ts
@@ -1,5 +1,31 @@
+import { ClsInputValue, vCls } from './vCls';
+import { vKeyless } from './vKeyless';
+import { vOn } from './vOn';
+import { VStyleItem, vStyle } from './vStyle';
+
 export * from './nextTick';
-export * from './vBind';
-export * from './vBindCls';
-export * from './vBindStyle';
+export * from './vCls';
+export * from './vKeyless';
 export * from './vOn';
+export * from './vStyle';
+
+class Adapter {
+  cls(value: ClsInputValue, mergeItem?: ClsInputValue) {
+    return vCls(value, mergeItem);
+  }
+
+  keyless(obj: Record<string, any>) {
+    return vKeyless(obj);
+  }
+
+  on(event: string, handler: any, fullEventObj?: boolean) {
+    return vOn(event, handler, fullEventObj);
+  }
+
+  style(target: VStyleItem, ...mergeItems: VStyleItem[]) {
+    return vStyle(target, mergeItems);
+  }
+}
+
+export const adapter = new Adapter();
+export const dir = adapter;
