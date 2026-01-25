77702e20613c3b813acaade6ccd1e8e3b4e4a3bd
smirk9581 <owendells@outlook.com>
2026-01-25 14:46:06 +0800

    re: 类名调用更改

--- packages/compiler-core/example/index.ts
+++ packages/compiler-core/example/index.ts
@@ -1,7 +1,7 @@
 import path from 'path';
-import { VuReactCompiler } from '../src';
+import { VuReact } from '../src/compiler';
 
-const compiler = new VuReactCompiler({
+const compiler = new VuReact({
   root: path.join(process.cwd(), 'example'),
   input: 'src',
 });
