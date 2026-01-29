c5ee1d932c5dde0564d2209c10221f4b71cc740e
smirk9581 <owendells@outlook.com>
2026-01-29 15:12:10 +0800

    chore: 排除 cac ora chokidar

--- packages/compiler-core/tsup.config.ts
+++ packages/compiler-core/tsup.config.ts
@@ -34,6 +34,9 @@ export default defineConfig({
     'minimatch',
     'xxhashjs',
     'postcss',
+    'cac',
+    'ora',
+    'chokidar',
   ],
   sourcemap: true,
   // 确保生成文件的扩展名与 package.json 中的 .mjs / .cjs 一致
