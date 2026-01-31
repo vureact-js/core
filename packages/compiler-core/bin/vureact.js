29f98b82d1c376af06b94c9c8a00338fc3b131b1
smirk9581 <owendells@outlook.com>
2026-01-31 12:44:12 +0800

    feat: 根据开发模式选择合适的 cli 导入位置

--- packages/compiler-core/bin/vureact.js
+++ packages/compiler-core/bin/vureact.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-import '../lib/cli.mjs';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const isDevEnv = fs.existsSync(path.join(__dirname, '../src'));
+
+if (isDevEnv) {
+  import('./development.js');
+} else {
+  import('./production.js');
+}
