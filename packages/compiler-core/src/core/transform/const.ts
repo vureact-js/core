f1b5c9da164c94f5738f5e79c0ee3fd10722b6bb
smirk9581 <owendells@outlook.com>
2026-02-07 17:01:08 +0800

    re: 移除 ReactCompProps、ReactCompEvents、ReactCompSlots、PropsIntersectionType，添加 __slots

--- packages/compiler-core/src/core/transform/const.ts
+++ packages/compiler-core/src/core/transform/const.ts
@@ -1,6 +1,3 @@
 export const __props = '__props';
 export const __emits = '__emits';
-export const ReactCompProps = 'ReactCompProps';
-export const ReactCompEvents = 'ReactCompEvents';
-export const ReactCompSlots = 'ReactCompSlots';
-export const PropsIntersectionType = 'PropsIntersectionType';
+export const __slots = '__slots';
