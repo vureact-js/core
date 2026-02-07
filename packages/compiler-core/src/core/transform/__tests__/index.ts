796fa630e41a124e5be29a739466bfeea980e30c
smirk9581 <owendells@outlook.com>
2026-02-07 17:26:43 +0800

    test: testPropsIface 方法用于测试 props ts 接口生成

--- packages/compiler-core/src/core/transform/__tests__/index.ts
+++ packages/compiler-core/src/core/transform/__tests__/index.ts
@@ -1,5 +1,7 @@
 import { transformBaseScript } from './baseScript';
 import { transformBaseSFC } from './baseSFC';
+import { testPropsIface } from './props-iface';
 
 transformBaseSFC();
 transformBaseScript();
+testPropsIface();
