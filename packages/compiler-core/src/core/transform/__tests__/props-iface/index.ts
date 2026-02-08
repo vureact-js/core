01d6fb7491a6d9a659ce6490ece04bed645528bd
smirk9581 <owendells@outlook.com>
2026-02-08 15:50:18 +0800

    test: 测试属于 props 的 TS 接口推导生成

---
+++ packages/compiler-core/src/core/transform/__tests__/props-iface/index.ts
@@ -0,0 +1,32 @@
+import { generate } from '@babel/generator';
+import { createCompilationCtx } from '@compiler/context';
+import { parse } from '@core/parse';
+import { transform } from '@core/transform';
+import { logger } from '@shared/logger';
+import { getDirname } from '@shared/path';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+export function testPropsIface() {
+  const __dirname = getDirname(import.meta.url);
+  const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
+
+  const ctx = createCompilationCtx();
+  ctx.init({ filename: './index.vue', source: content });
+
+  console.time('\ntestInterProps transform duration');
+
+  const ast = parse(content, ctx.data);
+  const result = transform(ast, ctx.data);
+
+  console.timeEnd('\ntestInterProps transform duration');
+
+  // console.log(result.script?.exports);
+
+  const code = result.script?.exports.map((node) => generate(node).code).join('');
+  console.log(code);
+
+  logger.printAll();
+}
+
+testPropsIface();
---
+++ packages/compiler-core/src/core/transform/__tests__/props-iface/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <slot name="header" title="title"></slot>
+    <slot></slot>
+    <slot name="footer" :count="1"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props<T> {
+  foo?: string;
+  bar: number;
+  dj: T;
+}
+
+const $$props = defineProps(['foo', 'bar']);
+
+// const $$props = defineProps({
+//   foo: String,
+//   bar: {
+//     type: Number,
+//     required: true,
+//   },
+// });
+
+// const $$props = defineProps<{
+//   foo?: string;
+//   bar: number;
+// }>();
+
+// const $$props = defineProps<Props>();
+
+// const $$props = defineProps<Props<any>>();
+
+// const $$props = defineProps<Props & { x: string }>();
+
+// const $$props = defineProps<Props | ({ a: string } | { b: number })>();
+
+interface Emits {
+  change: string;
+  (e: 'update', value: number): number;
+}
+
+const $$emits = defineEmits(['change', 'update', 'update:name']);
+
+// const $$emits = defineEmits<{
+//   change: [];
+//   update: [value: number];
+// }>();
+
+// const $$emits = defineEmits<{ (e: 'change'): void; (e: 'update', value: number): number }>();
+
+// const $$emits = defineEmits<Partial<{ (e: 'change'): void;}>>();
+
+// const $$emits = defineEmits<Emits>();
+
+interface Slots {
+  default?(): any;
+  header(props: { title: string }): any;
+  footer(props: { count: number }): any;
+}
+
+const $$slots = defineSlots<{
+  default?(): any;
+  header(props: { title: string }): any;
+  footer(props: { count: number }): any;
+}>();
+
+// const $$slots = defineSlots<Slots>();
+</script>
