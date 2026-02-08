5c2a49b0274f1edd00cd6772e4317bb252a459c8
smirk9581 <owendells@outlook.com>
2026-02-08 20:13:56 +0800

    feat: 新增一些常量

--- packages/compiler-core/src/consts/other.ts
+++ packages/compiler-core/src/consts/other.ts
@@ -1,1 +1,11 @@
-export const styleModule = 'styleModule';
+/** 函数组件的 props 参数名 */
+export const COMP_PROPS_NAME = '$$props';
+
+/** defineEmits 对应变量名 */
+export const EMITS_API_VAR_NAME = '$$emits';
+
+/** defineSlots 对应变量名 */
+export const SLOTS_API_VAR_NAME = '$$slots';
+
+/** 导入 style 模块所有使用的别名 */
+export const STYLE_MODULE_NAME = '$$style';
