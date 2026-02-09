d67519c75b24afdb61d669e4734b28b3b3d2701e
smirk9581 <owendells@outlook.com>
2026-02-09 10:04:22 +0800

    re: 迁移语法处理器调用

--- packages/compiler-core/src/core/transform/script/syntax-processor/index.ts
+++ packages/compiler-core/src/core/transform/script/syntax-processor/index.ts
@@ -1,6 +1,29 @@
 import { ParseResult, traverse } from '@babel/core';
 import { TraverseOptions } from '@babel/traverse';
 import { ICompilationContext } from '@compiler/context/types';
+import { optimizeFunction } from '../optimizations/function';
+import { processComputedApi } from './main-process/computed';
+import { processLifecycleApi } from './main-process/lifecycle';
+import { resolveProvideInject } from './main-process/provide-inject';
+import { processReactiveApi } from './main-process/reactive';
+import { processReadonlyApi } from './main-process/readonly';
+import { processWatchApi } from './main-process/watch';
+import { processWatchEffectApi } from './main-process/watchEffect';
+import { insertCSSImport } from './post-process/insert-css-import';
+import { insertRequiredImports } from './post-process/insert-required-imports';
+import { insertVModelEventHandlers } from './post-process/insert-vmodel-handlers';
+import { processReactiveValueUpdate } from './post-process/reactive-value-update';
+import { extractLocalStatements, splitScriptBlocks } from './post-process/script-blocks';
+import { resolveAsyncComponent } from './pre-process/resolve-async-component';
+import { resolvesDefineOptions } from './pre-process/resolve-define-options';
+import { resolveCompIProps, resolvePropsIface } from './pre-process/resolve-props-interface';
+import { resolveEmitsTopLevelTypes } from './pre-process/resolve-props-interface/resolve-emits';
+import {
+  resolveSlotsTopLevelTypes,
+  resolveTemplateSlotIface,
+} from './pre-process/resolve-props-interface/resolve-slot';
+import { stripReactiveValueSuffix } from './pre-process/strip-value-suffix';
+import { processTemplateNodeRef } from './pre-process/template-node-ref';
 
 interface ProcessorOptions {
   preprocess: ProcessorConfig;
@@ -14,11 +37,42 @@ interface ProcessorConfig {
   excludeBabel?: Array<(ctx: ICompilationContext, ast: ParseResult) => void>;
 }
 
-export function processVueSyntax(
-  ast: ParseResult,
-  ctx: ICompilationContext,
-  options: ProcessorOptions,
-) {
+export function processVueSyntax(ast: ParseResult, ctx: ICompilationContext) {
+  vueSyntaxProcessor(ast, ctx, {
+    preprocess: {
+      applyBabel: [
+        resolvesDefineOptions,
+        resolveEmitsTopLevelTypes,
+        resolveSlotsTopLevelTypes,
+        resolvePropsIface,
+        resolveAsyncComponent,
+        stripReactiveValueSuffix,
+        processTemplateNodeRef,
+      ],
+    },
+
+    process: {
+      applyBabel: [
+        processReactiveApi,
+        processReadonlyApi,
+        processComputedApi,
+        optimizeFunction,
+        processWatchApi,
+        processWatchEffectApi,
+        processLifecycleApi,
+        resolveProvideInject,
+      ],
+      excludeBabel: [resolveTemplateSlotIface, resolveCompIProps],
+    },
+
+    postprocess: {
+      applyBabel: [processReactiveValueUpdate, insertRequiredImports, splitScriptBlocks],
+      excludeBabel: [insertCSSImport, insertVModelEventHandlers, extractLocalStatements],
+    },
+  });
+}
+
+function vueSyntaxProcessor(ast: ParseResult, ctx: ICompilationContext, options: ProcessorOptions) {
   // 先执行普通处理函数，后执行依赖 babel 的处理函数
   const runExcludeThenApply = (cfg: ProcessorConfig) => {
     cfg.excludeBabel?.forEach((fn) => fn(ctx, ast));
