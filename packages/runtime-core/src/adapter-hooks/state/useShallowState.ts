33059f53aeca4bc23a50c4ddf6f5ca86427a9865
smirk9581 <owendells@outlook.com>
2026-02-10 19:57:26 +0800

    remove: 移除 useShallowState.ts

--- packages/runtime-core/src/adapter-hooks/state/useShallowState.ts
+++
@@ -1,8 +0,0 @@
-import { useState$ } from './useState$';
-
-/**
- * @see https://vureact-runtime.vercel.app/en/hooks/useShallowState
- */
-export function useShallowState<S>(initialState: S | (() => S)) {
-  return useState$(initialState, true) as [S, React.Dispatch<React.SetStateAction<S>>];
-}
