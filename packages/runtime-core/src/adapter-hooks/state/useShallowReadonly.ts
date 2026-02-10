1f3b6fb8dc005edf6fc2e02130bd062f1ba35c10
smirk9581 <owendells@outlook.com>
2026-02-10 19:57:40 +0800

    remove: 移除 useShallowReadonly.ts

--- packages/runtime-core/src/adapter-hooks/state/useShallowReadonly.ts
+++
@@ -1,8 +0,0 @@
-import { useReadonly } from './useReadonly';
-
-/**
- * @see https://vureact-runtime.vercel.app/en/hooks/useShallowReadonly
- */
-export function useShallowReadonly<T extends object>(initialState: T): Readonly<T> {
-  return useReadonly(initialState, true);
-}
