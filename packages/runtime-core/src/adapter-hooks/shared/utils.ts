d774adfee1d5d80ead0fed349894a75f6f4fb743
smirk9581 <owendells@outlook.com>
2026-02-11 11:23:39 +0800

    feat: 新增一些工具函数

--- packages/runtime-core/src/adapter-hooks/shared/utils.ts
+++ packages/runtime-core/src/adapter-hooks/shared/utils.ts
@@ -1,3 +1,8 @@
+import { freeze } from 'freeze-mutate';
+import { WrapRef } from 'index';
+import { klona } from 'klona';
+import { getVersion } from 'valtio/vanilla';
+import { PROXY_FLAG_KEY, PROXY_FLAG_VALUE } from './consts';
 import type { AsyncCallback } from './types';
 
 export function isAsyncFunc(fn: any): fn is AsyncCallback {
@@ -23,4 +28,29 @@ export function isPromise(obj: any): obj is Promise<any> {
 
 export function isPrimitive(value: unknown): boolean {
   return value === null || (typeof value !== 'object' && typeof value !== 'function');
+}
+
+export function isProxy(target: unknown): boolean {
+  return !isPrimitive(target) && getVersion(target) !== undefined;
+}
+
+// @ts-ignore
+export function isRefState<T>(value: T): value is WrapRef<T> {
+  return (value as any)[PROXY_FLAG_KEY] === PROXY_FLAG_VALUE.ref;
+}
+
+export function setProxyMeta(object: object, data: Record<string, any>) {
+  for (const key in data) {
+    (object as any)[key] = data[key];
+  }
+}
+
+export function freezeObject<T extends object>(
+  target: T,
+  options: { deep?: boolean; clone?: boolean } = {},
+): Readonly<T> {
+  const { clone = true, deep = true } = options;
+  const cloned = clone ? klona(target) : target;
+
+  return freeze(cloned, deep);
 }
