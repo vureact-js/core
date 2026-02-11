3be6a441a49ad4d31bab84298743451848dec8db
smirk9581 <owendells@outlook.com>
2026-02-11 21:59:51 +0800

    re: 增强处理逻辑

--- packages/runtime-core/src/adapter-hooks/state/useToRaw.ts
+++ packages/runtime-core/src/adapter-hooks/state/useToRaw.ts
@@ -1,9 +1,27 @@
-import { RAW_OBJ_KEY } from '../shared/consts';
+import { RAW_TARGET } from '../shared/consts';
+import { isProxy } from '../shared/proxy';
+import { isPrimitive } from '../shared/utils';
 
 /**
- * `toRaw()` can return the original object from proxies created by
- * `useReactive`, `useReadonly`, `useShallowReactive` or `useShallowReadonly`.
+ * Returns the raw (non-proxied) version of a reactive object if available.
+ *
+ * If the provided `target` is not a proxy or is a primitive, attempts to access its raw target via the `RAW_TARGET` symbol.
+ * Otherwise, returns the raw target if present, or the original target.
+ *
+ * @example
+ *
+ * const source = { count: 1 };
+ * const proxy = useReactive(source);
+ * const raw = useToRaw(proxy);
+ * // raw and source are strictly equal.
+ *
+ * @param target - The object to retrieve the raw version from.
+ * @returns The raw object if available, otherwise `undefined` or the original target.
  */
-export function useToRaw<T>(proxyObject: T): T {
-  return (proxyObject as any)[RAW_OBJ_KEY];
+export function useToRaw<T extends object>(target: T): T | undefined {
+  if (!isProxy(target) || isPrimitive(target)) {
+    return (target as any)?.[RAW_TARGET];
+  }
+
+  return (target as any)[RAW_TARGET] || target;
 }
