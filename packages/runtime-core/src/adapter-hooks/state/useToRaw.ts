d9df1ace42ee94a3d23c00911cac93c3c77f8253
smirk9581 <owendells@outlook.com>
2026-02-11 11:13:53 +0800

    re: toRaw 改为钩子 useToRaw

--- packages/runtime-core/src/adapter-hooks/state/useToRaw.ts
+++ packages/runtime-core/src/adapter-hooks/state/useToRaw.ts
@@ -1,9 +1,9 @@
-import { RAW_OBJ_KEY } from './consts';
+import { RAW_OBJ_KEY } from '../shared/consts';
 
 /**
  * `toRaw()` can return the original object from proxies created by
  * `useReactive`, `useReadonly`, `useShallowReactive` or `useShallowReadonly`.
  */
-export function toRaw<T>(proxyObject: T): T {
+export function useToRaw<T>(proxyObject: T): T {
   return (proxyObject as any)[RAW_OBJ_KEY];
 }
