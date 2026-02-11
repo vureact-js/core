e4ee4d3f461b11796ec9a9359fcfd5f7bd7ff48c
smirk9581 <owendells@outlook.com>
2026-02-11 11:15:20 +0800

    test: useToRaw advanced test suites

--- packages/runtime-core/src/adapter-hooks/__tests__/useToRaw.test.ts
+++ packages/runtime-core/src/adapter-hooks/__tests__/useToRaw.test.ts
@@ -1,12 +1,12 @@
 import { act, renderHook } from '@testing-library/react';
-import { toRaw } from '../shared/toRaw';
 import { useReactive } from '../state/useReactive';
+import { useToRaw } from '../state/useToRaw';
 
-describe('toRaw Advanced Test Suites', () => {
+describe('useToRaw Advanced Test Suites', () => {
   it('should return the original object from proxies created by useReactive', async () => {
     const source = { count: 1 };
     const { result } = renderHook(() => useReactive(source));
-    const raw = toRaw(result.current);
+    const raw = useToRaw(result.current);
 
     expect(result.current).not.toEqual(source);
     expect(raw).not.toEqual(result.current);
