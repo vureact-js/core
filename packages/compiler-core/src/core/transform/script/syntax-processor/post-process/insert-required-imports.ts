9ef560a6570d89fed7de98ae279e719cf02c06ed
smirk9581 <owendells@outlook.com>
2026-02-12 16:28:45 +0800

    re: 移除 removeVueRelatedModules 函数

--- packages/compiler-core/src/core/transform/script/syntax-processor/post-process/insert-required-imports.ts
+++ packages/compiler-core/src/core/transform/script/syntax-processor/post-process/insert-required-imports.ts
@@ -1,9 +1,7 @@
-import { NodePath, TraverseOptions } from '@babel/traverse';
+import { TraverseOptions } from '@babel/traverse';
 import * as t from '@babel/types';
 import { ICompilationContext } from '@compiler/context/types';
 import { RuntimeModules } from '@src/consts/runtimeModules';
-import { vueDerivedLibraries, vueDerivedPrefixes } from '@src/consts/vueDerivedLibraries';
-import { logger } from '@src/shared/logger';
 import { replaceVueSuffix } from '../../shared/replace-vue-suffix';
 
 export function insertRequiredImports(ctx: ICompilationContext): TraverseOptions {
@@ -33,7 +31,6 @@ export function insertRequiredImports(ctx: ICompilationContext): TraverseOptions
       }
 
       replaceVueSuffix(ctx, node.source);
-      removeVueRelatedModules(ctx, path);
     },
   };
 }
@@ -65,32 +62,4 @@ function createRequiredImports(ctx: ICompilationContext): t.ImportDeclaration[] 
   });
 
   return result;
-}
-
-// todo
-function removeVueRelatedModules(ctx: ICompilationContext, path: NodePath<t.ImportDeclaration>) {
-  const { source } = path.node;
-  const { scriptData, filename } = ctx;
-
-  if (!isBlacklistedVueImport(source.value)) return;
-
-  logger.warn(
-    'This package is an internal or derived Vue module that are incompatible with React, ' +
-      'the compiled output will not run correctly, ' +
-      'and you need to manually find and apply appropriate alternatives to handle them.',
-    {
-      source: scriptData.source,
-      file: filename,
-      loc: source.loc!,
-    },
-  );
-
-  path.addComment('leading', '!Incompatible: please manually replace.', true);
-}
-
-function isBlacklistedVueImport(source: string): boolean {
-  return (
-    vueDerivedLibraries.includes(source) ||
-    vueDerivedPrefixes.some((prefix) => source.startsWith(prefix))
-  );
 }
