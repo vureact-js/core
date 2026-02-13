baf2c472a753d331d5822dadf9d227370a38c5ed
smirk9581 <owendells@outlook.com>
2026-02-13 11:05:56 +0800

    feat: 增加常量 IS_ROOT

--- packages/runtime-core/src/adapter-hooks/shared/consts.ts
+++ packages/runtime-core/src/adapter-hooks/shared/consts.ts
@@ -1,6 +1,7 @@
-export const RAW_TARGET = '__raw_target';
-export const IS_REF_PROXY = '__is_ref_proxy';
-export const IS_REACTIVE_PROXY = '__is_reactive_proxy';
+export const RAW_TARGET = '__vr_raw';
+export const IS_ROOT = '__vr_is_root';
+export const IS_REF_PROXY = '__vr_is_ref';
+export const IS_REACTIVE_PROXY = '__vr_is_reactive';
 
 // Valtio 内部 proxy 指针
 export const VALTIO_PROXY_TARGET = '__valtio_proxy_target';
