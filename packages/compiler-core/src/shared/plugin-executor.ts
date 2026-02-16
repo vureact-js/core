cbc888006f0925db4e5b2a0ce8ec6930633eaf6f
smirk9581 <owendells@outlook.com>
2026-02-16 16:30:08 +0800

    re: executePlugins 函数增加上下文参数类型

--- packages/compiler-core/src/shared/plugin-executor.ts
+++ packages/compiler-core/src/shared/plugin-executor.ts
@@ -1,15 +1,10 @@
-import { ICompilationContext } from '@compiler/context/types';
 import { PluginRegister } from '@src/compiler';
 import kleur from 'kleur';
 
-export function executePlugins<T>(
-  map: PluginRegister<T> | undefined,
-  result: T,
-  ctx: ICompilationContext,
-) {
+export function executePlugins<T, C>(map: PluginRegister<T> | undefined, result: T, ctx: C) {
   for (const pluginName in map) {
     try {
-      map[pluginName]?.(result, ctx);
+      map[pluginName]?.(result, ctx as any);
     } catch (error: any) {
       console.error(kleur.red(`${kleur.bold('[Plugin]')} Error: ${pluginName} execution failed.`));
       console.error(kleur.red(error));
