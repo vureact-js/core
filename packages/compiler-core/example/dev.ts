b162f4961bc0824e449ffb4cf5b7ab0054b45e7d
smirk9581 <owendells@outlook.com>
2026-02-21 17:02:16 +0800

    remove: 移除 dev.ts

--- packages/compiler-core/example/dev.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { VuReact } from '../src/compiler';
-
-const compiler = new VuReact({
-  root: path.join(process.cwd(), 'example'),
-  input: 'src',
-});
-
-compiler.execute();
