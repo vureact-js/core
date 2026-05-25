eacf2965498053c44dd50203a4a339d49e2059da
smirk9581 <owendells@outlook.com>
2026-05-25 10:51:15 +0800

    test(compiler-core): 新增 vue-type-as-any 测试用例

---
+++ packages/compiler-core/src/core/transform/__tests__/vue-type-as-any/index.ts
@@ -0,0 +1,32 @@
+import { createCompilationCtx } from '@compiler/context';
+import { parseSFC } from '@core/parse';
+import { logger } from '@shared/logger';
+import { getDirname } from '@shared/path';
+import { generate, transform } from '@src/core';
+import { readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+
+function testVueTypeAsAny() {
+  const __dirname = getDirname(import.meta.url);
+  const content = readFileSync(path.resolve(__dirname, './index.vue'), 'utf-8');
+
+  const ctx = createCompilationCtx();
+  ctx.init({ filename: './index.vue', source: content });
+
+  console.time('testVueTypeAsAny transform duration');
+  console.log();
+
+  const ast = parseSFC(content, ctx.data);
+  const result = transform(ast, ctx.data);
+
+  console.timeEnd('testVueTypeAsAny transform duration');
+  console.log();
+
+  const code = generate(result, ctx.data).code;
+
+  writeFileSync(path.join(__dirname, './preview.tsx'), code, 'utf-8');
+
+  logger.printAll();
+}
+
+testVueTypeAsAny();
