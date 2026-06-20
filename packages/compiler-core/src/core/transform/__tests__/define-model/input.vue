9cebbdb221c3ccab2e6f822b83219f72a9351dfb
smirk9581 <owendells@outlook.com>
2026-06-20 16:52:16 +0800

    test(compiler-core): 完善 defineModel 测试用例

--- packages/compiler-core/src/core/transform/__tests__/define-model/input.vue
+++ packages/compiler-core/src/core/transform/__tests__/define-model/input.vue
@@ -3,19 +3,20 @@
 // @ts-nocheck
 
 // 声明 "state" prop，由父组件通过 v-model:state 使用
-const state = defineModel('state');
+const state = defineModel<string>('state');
 
 // 声明带选项的 "modelValue" prop，由父组件通过 v-model 使用
-const modelValue = defineModel({ type: String, default: 'xxx' });
+const modelValue = defineModel({ default: 'xxx' });
 
 // 声明带选项的 "count" prop，由父组件通过 v-model:count  使用
-const count = defineModel<number>('count', { type: Number, default: 0 });
+const count = defineModel<number>('count', { type: Number, default: 0, required: true });
+
+// 在被修改时，触发 "update:state" 事件
+state.value = 'hello';
 
 const inc = () => {
   // 在被修改时，触发 "update:count" 事件
   count.value++;
-  // 在被修改时，触发 "update:modelValue" 事件
-  modelValue.value.substring(0, count.value);
 };
 </script>
 
